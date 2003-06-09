596c74555743edb199db8b4242af8ad9baaa6a55
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2003-06-09 13:30:46 +0000

    This module contains IO used in saving an answer to a file. --Mike
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@1073 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ lib/AnswerIO.pm
@@ -0,0 +1,62 @@
+#!/usr/math/bin/perl -w
+
+=head1 NAME
+
+	AnswerIO.pm
+
+=head1 SYNPOSIS
+
+This is not really an object, but it gives us a place to IO used by answer
+macros.
+
+
+
+=head1 DESCRIPTION
+
+
+=head2 Examples: 
+
+
+
+=cut
+
+
+BEGIN {
+	be_strict(); # an alias for use strict.  This means that all global variable must contain main:: as a prefix.
+}
+
+package AnswerIO;
+
+
+
+# Code for saving Answers to a file
+# function, not a method
+# Code in .pm files can access the disk.
+
+
+sub saveAnswerToFile {
+	  my $logFileID = shift;
+      my $string = shift;
+      # We want to allow acces only to predetermined files
+      # We accomplish this by translating legal IDs into a file name
+      
+      my $rh_allowableFiles = {preflight => 'preflight.log',
+      
+      	};
+      my $error=undef;
+      my $logFileName = $rh_allowableFiles->{$logFileID};
+      if ( defined($logFileName) ) {
+      	my $accessLog = Global::getCourseLogsDirectory().$logFileName;
+      	#$error = "access Log is $accessLog";
+      	#$error .="string is $string";
+      	open(LOG, ">>$accessLog") or $error.= "Can't open course access log $accessLog";
+      	print LOG $string;  #no format is forced on data.
+      	close(LOG);
+      } else {
+      	$error = "Error: The file ID $logFileID is not recognized.";
+      }
+	return $error ;
+}
+
+
+1;
