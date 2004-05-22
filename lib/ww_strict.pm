fc43762061d55105e318c5b84a1efab724963588
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-05-22 21:34:58 +0000

    Moved ww_strict.pm back to be identical with strict.pm from perl 5.6
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@2163 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/ww_strict.pm
+++ lib/ww_strict.pm
@@ -89,36 +89,21 @@ refs => 0x00000002,
 subs => 0x00000200,
 vars => 0x00000400
 );
-#require Carp;
-# sub bits {
-#     my $bits = 0;
-#     foreach my $s (@_){ $bits |= $bitmask{$s} || 0; };
-#     $bits;
-# }
+
 sub bits {
     my $bits = 0;
-    my @wrong;
-    foreach my $s (@_) {
-	push @wrong, $s unless exists $bitmask{$s};
-        $bits |= $bitmask{$s} || 0;
-    }
-    if (@wrong) {
-#        require Carp;
-
-        Carp::croak("Unknown 'strict' tag(s) '@wrong'");
-    }
+    foreach my $s (@_){ $bits |= $bitmask{$s} || 0; };
     $bits;
 }
-my $default_bits = bits(qw(refs subs vars));
 
 sub import {
     shift;
-    $^H |= @_ ? bits(@_) : $default_bits;
+    $^H |= bits(@_ ? @_ : qw(refs subs vars));
 }
 
 sub unimport {
     shift;
-    $^H &= ~ (@_ ? bits(@_) : $default_bits);
+    $^H &= ~ bits(@_ ? @_ : qw(refs subs vars));
 }
 
 1;
