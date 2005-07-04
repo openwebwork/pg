1bb62fb02478f0ee1e2ab12fe71e4f35a09d3c2c
dpvc <dpvc@c0722133-6baf-4dd8-8699-98d999cd4f06>
2005-07-04 20:12:22 +0000

    This is an attempt at making the traditional answer checkers call the new Parser in place of their original ones.  That is, if you follow the instructions in the README file, then problems that use std_num_cmp(), fun_cmp(), etc. will really be using the new Parser instead of the original PGanswermacros.pl versions.
    The old answer checkers are still available, and can be switched back
    on a site-wide, course-wide, or problem-by-problem basis.  See the
    README for details.
    
    
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@3347 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ lib/Parser/Legacy/LimitedNumeric.pm
@@ -0,0 +1,87 @@
+##########################################################
+#
+#  Implements a context in which numbers can be entered,
+#  but no operations are permitted between them.
+#
+#  There are two versions:  one a number with no operations,
+#  and one for fractions of integers.  Select them using
+#  one of the following commands:
+#
+#      Context("LimiteNumeric");
+#      Context("LimitedNumeric-Fraction");
+#
+
+
+#
+#  Minus can only appear in front of a number
+#
+package Parser::Legacy::LimitedNumeric::UOP::minus;
+our @ISA = qw(Parser::UOP::minus);
+
+sub _check {
+  my $self = shift;
+  $self->SUPER::_check;
+  my $uop = $self->{def}{string} || $self->{uop};
+  $self->Error("You can only use '$uop' with (non-negative) numbers")
+    unless $self->{op}->class =~ /Number|DIVIDE/;
+}
+
+sub class {'MINUS'};
+
+
+#
+#  Divides can only appear between numbers or a negative
+#  number and a number
+#
+package Parser::Legacy::LimitedNumeric::BOP::divide;
+our @ISA = qw(Parser::BOP::divide);
+
+sub _check {
+  my $self = shift;
+  $self->SUPER::_check;
+  my $bop = $self->{def}{string} || $self->{bop};
+  $self->Error("You can only use '$bop' between (non-negative) numbers")
+    unless $self->{lop}->class =~ /Number|MINUS/ &&
+           $self->{rop}->class eq 'Number';
+}
+
+sub class {'DIVIDE'};
+
+
+package Parser::Legacy::LimitedNumeric;
+
+#
+#  LimitedNumeric context uses the modified minus
+#  and removes all other operatots, functions and parentheses
+#
+my $context = $Parser::Context::Default::context{Numeric}->copy;
+$Parser::Context::Default::context{'LimitedNumeric'} = $context;
+$context->operators->set('u-' => {class => 'Parser::Legacy::LimitedNumeric::UOP::minus'});
+$context->operators->undefine(
+   '+', '-', '*', '* ', ' *', ' ', '/', '/ ', ' /', '^', '**',
+   'U', '.', '><', 'u+', '!', '_', ',',
+);
+$context->parens->undefine('|','{','(','[');
+$context->functions->disable('All');
+
+#
+#  For the Fraction versions, allow the modified division, and
+#  make sure numbers are just integers
+#
+$context = $Parser::Context::Default::context{Numeric}->copy;
+$Parser::Context::Default::context{'LimitedNumeric-Fraction'} = $context;
+$context->operators->set(
+  'u-' => {class => 'Parser::Legacy::LimitedNumeric::UOP::minus'},
+  '/'  => {class => 'Parser::Legacy::LimitedNumeric::BOP::divide'},
+  ' /' => {class => 'Parser::Legacy::LimitedNumeric::BOP::divide'},
+  '/ ' => {class => 'Parser::Legacy::LimitedNumeric::BOP::divide'},
+);
+$context->operators->undefine(
+   '+', '-', '*', '* ', ' *', ' ', '^', '**',
+   'U', '.', '><', 'u+', '!', '_', ',',
+);
+$context->parens->undefine('|','{','[');
+$context->functions->disable('All');
+Parser::Number::NoDecimals($context);
+
+1;
---
+++ lib/Parser/Legacy/NumberWithUnits.pm
@@ -0,0 +1,118 @@
+######################################################################
+#
+#  This is a Parser class that implements a number with units.
+#  It is a temporary version until the Parser can handle it
+#  directly.
+#
+
+package Parser::Legacy::NumberWithUnits;
+our @ISA = qw(Value::Real);
+
+sub new {
+  my $self = shift; my $class = ref($self) || $self;
+  my $num = shift; my $units = shift;
+  Value::Error("You must provide a number") unless defined($num);
+  ($num,$units) = $num =~ m/^(.*)\s+(\S*)$/ unless $units;
+  Value::Error("You must provide units for your number")
+    unless $units;
+  $num = Value::makeValue($num);
+  Value::Error("A number with units must be a constant, not ".lc(Value::showClass($num)))
+    unless Value::isReal($num);
+  my %Units = getUnits($units);
+  Value::Error($Units{ERROR}) if ($Units{ERROR});
+  $num->{units} = $units;
+  $num->{units_ref} = \%Units;
+  $num->{isValue} = 1;
+  bless $num, $class;
+}
+
+#
+#  Add the units to the string value
+#
+sub string {
+  my $self = shift;
+  $self->SUPER::string . " " . $self->{units};
+}
+
+#
+#  Add the units to the TeX value
+#
+sub TeX {
+  my $self = shift;
+  $self->SUPER::TeX . '\ ' . TeXunits($self->{units});
+}
+
+
+sub cmp_class {'a Number with Units'};
+
+#
+#  Replace the cmp_parse with one that removes the units
+#  from the student answer and checks them.  The answer
+#  value is adjusted by the factors, and then checked.
+#  Finally, the units themselves are checked.
+#
+sub cmp_parse {
+  my $self = shift; my $ans = shift;
+  #
+  #  Check that the units are defined and legal
+  #
+  my ($num,$units) = $ans->{student_ans} =~ m/^(.*)\s+(\S*)$/;
+  unless (defined($num) && $units) {
+    $self->cmp_Error($ans,"Your answer doesn't look like a number with units");
+    return $ans;
+  }
+  my %Units = getUnits($units);
+  if ($Units{ERROR}) {$self->cmp_Error($ans,$Units{ERROR}); return $ans}
+  #
+  #  Check the numeric part of the answer
+  #   and adjust the answer strings
+  #
+  $ans->{correct_value} *= $self->{units_ref}{factor}/$Units{factor};
+  $ans->{student_ans} = $num;
+  $ans = $self->SUPER::cmp_parse($ans);
+  $ans->{student_ans} .= " " . $units;
+  $ans->{preview_text_string}  .= " ".$units;
+  $ans->{preview_latex_string} .= '\ '.TeXunits($units);
+  #
+  #  If there is not already a message, check the units
+  #
+  return $ans unless $ans->{ans_message} eq '';
+  foreach my $funit (keys %{$self->{units_ref}}) {
+    next if $funit eq 'factor';
+    next if $self->{units_ref}{$funit} == $Units{$funit};
+    $self->cmp_Error($ans,"The units for your answer are not correct")
+      unless $ans->{isPreview};
+    $ans->score(0); last;
+  }
+  return $ans;
+}
+
+#
+#  Convert units to TeX format
+#  (fix superscripts, put terms in \rm,
+#   and make a \frac out of fractions)
+#
+sub TeXunits {
+  my $units = shift;
+  $units =~ s/\^\(?([-+]?\d+)\)?/^{$1}/g;
+  $units =~ s/\*/\\,/g;
+  return '{\rm '.$units.'}' unless $units =~ m!^(.*)/(.*)$!;
+  my $displayMode = WeBWorK::PG::Translator::PG_restricted_eval(q!$main::displayMode!);
+  return '\frac{'.$1.'}{'.$2.'}' if ($displayMode eq 'HTML_tth');
+  return '\frac{\rm\mathstrut '.$1.'}{\rm\mathstrut '.$2.'}';
+}
+
+#
+#  Get the units hash and fix up the errors
+#
+sub getUnits {
+  my $units = shift;
+  my %Units = Units::evaluate_units($units);
+  if ($Units{ERROR}) {
+    $Units{ERROR} =~ s/ at ([^ ]+) line \d+(\n|.)*//;
+    $Units{ERROR} =~ s/^UNIT ERROR:? *//;
+  }
+  return %Units;
+}
+
+1;
