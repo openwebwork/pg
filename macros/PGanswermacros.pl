73b6d2bed3c9708f9cf956c7dd2ea9459188b154
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2005-07-20 18:30:59 +0000

    Added a modification of the grades that shows where the state_summary_msg can be added.  This message is printed at the bottom of each problem page.  The current default is a blank message, which means that the default message defined in Problem.pm is printed.
    There is currently no way to turn the message off entirely.  Perhaps
    on the next revision.
    
    
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@3393 c0722133-6baf-4dd8-8699-98d999cd4f06

--- macros/PGanswermacros.pl
+++ macros/PGanswermacros.pl
@@ -4312,6 +4312,9 @@ sub std_problem_grader {
 
 	$problem_state{num_of_correct_ans}++ if	$allAnswersCorrectQ	== 1;
 	$problem_state{num_of_incorrect_ans}++ if $allAnswersCorrectQ == 0;
+	
+	$problem_state{state_summary_msg} = '';  # an HTML formatted message printed at the bottom of the problem page
+	
 	(\%problem_result, \%problem_state);
 }
 
@@ -4411,6 +4414,8 @@ sub std_problem_grader2 {
 	if ($record_problem_attempt	== 1) {
 		$problem_state{num_of_correct_ans}++ if	$allAnswersCorrectQ	== 1;
 		$problem_state{num_of_incorrect_ans}++ if $allAnswersCorrectQ == 0;
+		$problem_state{state_summary_msg} = '';  # an HTML formatted message printed at the bottom of the problem page
+	
 	}
 	else {
 		$problem_result{show_partial_correct_answers} =	0 ;	 # prevent partial correct answers from	being shown	for	syntax errors.
@@ -4484,6 +4489,9 @@ sub avg_problem_grader {
 
 	$problem_state{num_of_correct_ans}++ if	$total == $count;
 	$problem_state{num_of_incorrect_ans}++ if $total < $count ;
+	
+	$problem_state{state_summary_msg} = '';  # an HTML formatted message printed at the bottom of the problem page
+	
 	warn "Error	in grading this	problem	the	total $total is	larger than	$count"	if $total >	$count;
 	(\%problem_result, \%problem_state);
 }
