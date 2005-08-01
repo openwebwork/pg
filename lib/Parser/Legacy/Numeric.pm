71d23eb750de5781ed7fb94181f0652584171f55
dpvc <dpvc@c0722133-6baf-4dd8-8699-98d999cd4f06>
2005-08-01 15:14:20 +0000

    Fixed a bug that would cause step() to produce an error when called on a constant value.  Also, added fact() function that was defined in Algparser but not in Parser (which uses x! notation).
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@3446 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/Parser/Legacy/Numeric.pm
+++ lib/Parser/Legacy/Numeric.pm
@@ -1,19 +1,28 @@
 ##########################################################
 #
-#  Implements a context in which the "step" function
-#  is defined.  This was defined in the old AlgParser,
-#  but not in the Parser's standard Numeric context.
+#  Implements a context in which the "step" and "fact" 
+#  functions are defined.  These were defined in the old
+#  AlgParser, but are not in the Parser's standard
+#  Numeric context.
 #
-#  Warning:  since step is already defined in PGauxiliarymacros.pl
-#  we can't redefine it here, so you can't use step(formula) to
-#  automatically generate Formula objects, as you can with
-#  all the other functions.
+#  Warning:  since step and fact already are defined in
+#  PGauxiliarymacros.pl we can't redefine them here, so you
+#  can't use step(formula) or fact(formula) to automatically
+#  generate Formula objects, as you can with all the other
+#  functions.  Since this context is for compatibility with
+#  old problems that didn't know about Formula objects
+#  anyway, that should not be a problem.
+#
 
 package Parser::Legacy::Numeric;
 our @ISA = qw(Parser::Function::numeric);
-sub step {shift; main::step((shift)->value)}
+sub step {shift; do_step(shift)}; sub do_step {Value::pgCall('step',@_)}
+sub fact {shift; do_fact(shift)}; sub do_fact {Value::pgCall('fact',@_)}
 
 my $context = $Parser::Context::Default::context{Numeric}->copy;
 $Parser::Context::Default::context{LegacyNumeric} = $context;
-$context->functions->add(step => {class => 'Parser::Legacy::Numeric'});
+$context->functions->add(
+  step => {class => 'Parser::Legacy::Numeric', perl => 'Parser::Legacy::Numeric::do_step'},
+  fact => {class => 'Parser::Legacy::Numeric', perl => 'Parser::Legacy::Numeric::do_fact'},
+);
 
