713362364a2f31edc33d0dbd9ec7977c3106d24f
apizer <apizer@c0722133-6baf-4dd8-8699-98d999cd4f06>
2005-11-29 21:25:25 +0000

    Add underscores to the anonymous unique identifier for questionnaires in WW 2 by using ${setNumber}_${courseName}_$psvnNumber
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@3781 c0722133-6baf-4dd8-8699-98d999cd4f06

--- macros/PGanswermacros.pl
+++ macros/PGanswermacros.pl
@@ -3149,7 +3149,7 @@ sub anstext {
 	my $ans_eval    = sub {
 				 my	$text =	shift;
 				 $text = ''	unless defined($text);
-				 my	$new_text =	"\n$setNumber$courseName$psvnNumber-Problem-$probNum-Question-$num:\n $text "; #	modify entered text
+				 my	$new_text =	"\n${setNumber}_${courseName}_$psvnNumber-Problem-$probNum-Question-$num:\n $text "; #	modify entered text
 				 my	$out = &$ans_eval_template($new_text);			 # standard	evaluator
 				 #warn "$QUESTIONNAIRE_ANSWERS";
 				 $out->{student_ans} = escapeHTML($text);  #	restore	original entered text
