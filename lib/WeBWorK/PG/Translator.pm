2d1cdd7c56462303fdc3659ba55f2fcd9e6dea67
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2006-11-15 16:50:05 +0000

    SECURITY: tighter constraints on which macro files can be loaded without restriction. Previously, any file that *contained* the strings PG.pl, dangerousMacros.pl, or IO.pl would be loaded with an empty opmask.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@4654 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/PG/Translator.pm
+++ lib/WeBWorK/PG/Translator.pm
@@ -388,14 +388,14 @@ sub pre_load_macro_files {
 #     
 			my $store_mask; 
 			my $unrestricted_files  = "PG.pl|dangerousMacros.pl|IO.pl";
-			if ($fileName =~ /$unrestricted_files/) {
+			if ($fileName =~ /^$unrestricted_files$/) {
 	        	$store_mask = $cached_safe_cmpt->mask();
 				$cached_safe_cmpt ->mask(Opcode::empty_opset());
 	        } 			
 			$cached_safe_cmpt -> reval('BEGIN{push @main::__eval__,__FILE__}; package main; ' .$string);
 			warn "preload Macros: errors in compiling $macro_file_name:<br> $@" if $@;
 			$self->{envir}{__files__}{$cached_safe_cmpt->reval('pop @main::__eval__')} = $filePath;
-			if ($fileName =~ /$unrestricted_files/) {
+			if ($fileName =~ /^$unrestricted_files$/) {
 	        	$cached_safe_cmpt ->mask($store_mask);
 	        	warn "mask restored after $fileName" if $debugON;
 	        }
