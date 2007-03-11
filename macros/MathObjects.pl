296faeb1756f1f2485a8cb3e3160f7871598d556
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-03-11 16:11:20 +0000

    This file loads Parser.pl.   It is part of a campaign to replace the term "parser object" with MathObject  which should sound more intuitive for those who are unaware of the development history of the PG language.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@4867 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ macros/MathObjects.pl
@@ -0,0 +1,9 @@
+# This file loads Parser.pl which in turn loads Value.pl
+# The purpose of this file is to encourage the use of the name MathObjects
+# instead of Parser (which is not as intuitive for those who don't know
+# the history.
+
+# It may later be used for other purposes as well.
+
+loadMacros("Parser.pl");
+
