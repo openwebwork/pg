4c937ddb846334373af0adcb3012d5daa77b7bea
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-03-14 00:35:38 +0000

    Added comments about working with complex variables
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@4877 c0722133-6baf-4dd8-8699-98d999cd4f06

--- macros/answerCustom.pl
+++ macros/answerCustom.pl
@@ -41,6 +41,11 @@ sub _answerCustom_init {}; # don't reload this file
  #  multiple times with various combinations of student and professor's
  #  answers in order to find a correct match.
  #
+ *  Note: If you want a correct answer whose class is a complex variable
+ #  to check a real number entry you will have to set both sameClass and
+ #  sameLength to 0 since a complex number has length 2 and a real number
+ #  has length 1.
+ # 
  #  The checker routine will be passed the correct answer, the
  #  student's answer, and the answer evaluator object, in that order.
  #
