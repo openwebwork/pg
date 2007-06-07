d49ac6fcb594e2593fa5a5d387f12e2b3b2e4f7b
sroubos <sroubos@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-06-07 17:42:29 +0000

    Make readable by POD documentation from the web
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@4985 c0722133-6baf-4dd8-8699-98d999cd4f06

--- macros/contextPeriodic.pl
+++ macros/contextPeriodic.pl
@@ -1,16 +1,25 @@
-######################################################################
-#
-#   This file is no longer needed, as these features have been added to the
-#   Real and Complex MathObject classes.
-#
-#	Usage:
-#		Context("Numeric");
-#		$a = Real("pi/2")->with(period=>pi);
-#		$a->cmp         # will match pi/2, 3pi/2 etc.
-#
-#	Usage:
-#		Context("Complex");
-#		$z0 = Real("i^i")->with(period=>2pi, logPeriodic=>1);
-#		$z0->cmp        # will match exp(i*(ln(1) + Arg(pi/2) + 2k pi))
-#
-######################################################################
+
+
+
+
+=head1 DESCRIPTION
+
+ ######################################################################
+ #
+ #   This file is no longer needed, as these features have been added to the
+ #   Real and Complex MathObject classes.
+ #
+ #	Usage:
+ #		Context("Numeric");
+ #		$a = Real("pi/2")->with(period=>pi);
+ #		$a->cmp         # will match pi/2, 3pi/2 etc.
+ #
+ #	Usage:
+ #		Context("Complex");
+ #		$z0 = Real("i^i")->with(period=>2pi, logPeriodic=>1);
+ #		$z0->cmp        # will match exp(i*(ln(1) + Arg(pi/2) + 2k pi))
+ #
+ ######################################################################
+
+=cut
+
