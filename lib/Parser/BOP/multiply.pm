bc96ea37d7f4f1691a4c1d278cbb8083eb3d9fdd
dpvc <dpvc@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-06-21 23:29:10 +0000

    Don't use \cdot with things like 3i.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@5013 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/Parser/BOP/multiply.pm
+++ lib/Parser/BOP/multiply.pm
@@ -93,7 +93,9 @@ sub TeX {
 
   my $left  = $self->{lop}->TeX($bop->{precedence},$bop->{leftparens},'left',$outerRight);
   my $right = $self->{rop}->TeX($bop->{precedence},$bop->{rightparens},'right');
-  $mult = $cdot if $right =~ m/^\d/ || ($left =~ m/\d+$/ && $self->{rop}{isConstant} && $self->{rop}->type eq 'Number');
+  $mult = $cdot if $right =~ m/^\d/ ||
+     ($left =~ m/\d+$/ && $self->{rop}{isConstant} &&
+      $self->{rop}->type eq 'Number' && $self->{rop}->class ne 'Constant');
   $TeX = $left.$mult.$right;
 
   $TeX = '\left('.$TeX.'\right)' if $addparens;
