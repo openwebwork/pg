a343dd7e6272cac2611cc7757591949140de4f10
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-08-10 18:36:10 +0000

    forward-port (sh002i): Added a version of PGstandard.pl which loads PG.pl, PGbasicmacros.pl, PGanswermacros.pl, and PGauxiliaryFunctions.pl. It also includes POD docs!
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@5310 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ macros/PGstandard.pl
@@ -0,0 +1,34 @@
+=head1 NAME
+
+PGstandard.pl - Load standard PG macro packages.
+
+=head1 SYNOPSIS
+
+ loadMacros('PGstandard.pl');
+
+=head1 DESCRIPTION
+
+PGstandard.pl loads the following macro files:
+
+=over
+
+=item * PG.pl
+
+=item * PGbasicmacros.pl
+
+=item * PGanswermacros.pl
+
+=item * PGauxiliaryFunctions.pl
+
+=back
+
+=cut
+
+loadMacros(
+	"PG.pl",
+	"PGbasicmacros.pl",
+	"PGanswermacros.pl",
+	"PGauxiliaryFunctions.pl",
+);
+
+1;
