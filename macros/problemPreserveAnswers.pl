cfdf100c1fa8c630aea0414d229a4dfc5007545d
dpvc <dpvc@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-08-17 01:59:33 +0000

    This is a temporary hack that allows sticky answers to preserve all the characters the student typed.  PGbasicmacros.pl currently removes several characters, including $, which is needed for monitary answers.  Eventually, PGbasicmacros.pl should be fixed, but for now this will work, and it should not cause trouble after PGbasicmacros.pl is fixed.  In the end, this file can be emptied, then it should issue a warning message, and finally it can be removed.
    The hack is to copy the inputs_ref and modify the answers so that the
    PGbasicmacros.pl macros will not remove those characters (the key is
    to convert them to HTML entities instead).  Then after the problem has
    been processed, we put the original answers back so that the answer
    checkers will work properly on them.  This is accomplished by
    replacing ENDDOCUMENT with a new routine that first puts back the
    answers and then calls the old ENDDOCUMENT.  This has the unwanted
    side effect of putting error messages into the error log, but I can't
    seem to work around that.
    
    
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@5342 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ macros/problemPreserveAnswers.pl
@@ -0,0 +1,73 @@
+
+=pod
+
+######################################################################
+#
+#  This file implements a fragile hack to overcome a problem with
+#  PGbasicmacros.pl, which removes special characters from student
+#  answers (in order to prevent EV3 from mishandling them).
+#
+#  Unfortunately, this means that "sticky" answers will lose
+#  those characters, which makes it very difficult to answer
+#  problems with more than one answer when the student wants
+#  to submit several times while working on later parts.
+#
+#  The real fix to to rewrite PGbasicmacros.pl to handle
+#  this better, but this hack will handle the situation for
+#  now until that can be accomplished.
+#
+#  To use this hack, simply load the file using
+#
+#    loadMacros("problemPreserveAnswers.pl");
+#
+#  at the top of your PG file.
+#
+
+=cut
+
+sub _problemPreserveAnswers_init {PreserveAnswers::Init()}
+
+package PreserveAnswers;
+
+#
+#  Escape the specials in answers, and then
+#  override ENDDOCUMENT() to restore the answers
+#  to their original values.
+#
+sub Init {
+  PreserveAnswers::EscapeAnswers();
+  $PreserveAnswers::ENDDOCUMENT = \&main::ENDDOCUMENT;
+  main::PG_restricted_eval
+    ('sub ENDDOCUMENT {PreserveAnswers::RestoreAnswers(); &$PreserveAnswers::ENDDOCUMENT(@_)}');
+}
+
+#
+#  This is a fragile hack to prevent PG from removing the
+#  dollar signs in currency answers (and everything else as well).
+#  PGbasicmacros.pl needs to be fixed to allow this without
+#  having to do this terrible hack.
+#
+sub EscapeAnswers {
+  my $original = $main::inputs_ref_orig = {%{$main::inputs_ref}};
+  my $inputs   = $main::inputs_ref;
+  foreach my $id (keys %{$original}) {
+    my $value = $original->{$id};
+    next if !defined($value) || ref($value);
+    $value =~ s/([\\\$@\`])/EscapeHTMLchar($1)/ge;
+    $inputs->{$id} = $value;
+  }
+}
+
+sub EscapeHTMLchar {main::spf(ord(shift),"&#x%02X;")}
+
+sub RestoreAnswers {
+  my $original = $main::inputs_ref_orig;
+  my $inputs   = $main::inputs_ref;
+  foreach my $id (keys %{$original}) {$inputs->{$id} = $original->{$id}}
+}
+
+our $ENDDOCUMENT;
+
+######################################################################
+
+1;
