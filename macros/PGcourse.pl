8a7a191140f6d293ca353a5395d4347a2b7ff7b2
dpvc <dpvc@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-08-31 13:41:16 +0000

    Add the PGcourse.pl file, since we are encouraging authors to include this in theie loadMacros() calls.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@5458 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ macros/PGcourse.pl
@@ -0,0 +1,12 @@
+
+=head1 PGcourse.pl
+
+ #
+ #  Do course-specific initialization here.
+ #  (E.g. loading source.pl to get "show source" buttons
+ #  for example courses, and so on).
+ #
+
+=cut
+
+1;
