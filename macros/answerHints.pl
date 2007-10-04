e5ebce1363005cd1369dce5c474b7e68ff1b5f44
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-10-04 16:40:49 +0000

    added standard copyright/license header
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@5556 c0722133-6baf-4dd8-8699-98d999cd4f06

--- macros/answerHints.pl
+++ macros/answerHints.pl
@@ -1,4 +1,18 @@
-sub _answerHints_init {}
+################################################################################
+# WeBWorK Online Homework Delivery System
+# Copyright © 2000-2007 The WeBWorK Project, http://openwebwork.sf.net/
+# $CVSHeader: webwork2/lib/WeBWorK.pm,v 1.100 2007/08/13 22:59:53 sh002i Exp $
+# 
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of either: (a) the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version, or (b) the "Artistic License" which comes with this package.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
+# Artistic License for more details.
+################################################################################
 
 =head1 AnswerHints()
 
@@ -87,6 +101,8 @@ Example:
 	)));
 
 =cut
+
+sub _answerHints_init {}
 
 sub AnswerHints {
   return (sub {
