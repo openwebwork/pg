d6928d814b617c774beda978f1964bc92ba68417
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2009-06-25 23:33:26 +0000

    add copyright to PGbasicmacros.pl
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@6059 c0722133-6baf-4dd8-8699-98d999cd4f06

--- macros/PGbasicmacros.pl
+++ macros/PGbasicmacros.pl
@@ -1,9 +1,19 @@
+################################################################################
+# WeBWorK Program Generation Language
+# Copyright  2000-2007 The WeBWorK Project, http://openwebwork.sf.net/
+# $CVSHeader$
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of either: (a) the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version, or (b) the "Artistic License" which comes with this package.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
+# Artistic License for more details.
+################################################################################
 
-
-####################################################################
-# Copyright @ 1995-1998 University of Rochester
-# All Rights Reserved
-####################################################################
 
 =head1 NAME
 
