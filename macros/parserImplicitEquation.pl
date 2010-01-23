15a8059ec385049c83c64162c41685406dcaf8e8
dpvc <dpvc@c0722133-6baf-4dd8-8699-98d999cd4f06>
2010-01-23 16:39:35 +0000

    Fixed wrong default value in comments
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@6185 c0722133-6baf-4dd8-8699-98d999cd4f06

--- macros/parserImplicitEquation.pl
+++ macros/parserImplicitEquation.pl
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2007 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader$
+# $CVSHeader: pg/macros/parserImplicitEquation.pl,v 1.14 2009/06/25 23:28:44 gage Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -106,7 +106,7 @@ the tested function is zero.
 
 the minimum tolerance allowed.
 
-=item C<S<< ImplicitAbsoluteMaxTolerance => 1E-3 >>>
+=item C<S<< ImplicitAbsoluteMaxTolerance => 1 >>>
 
 the maximum tolerance allowed.
 
