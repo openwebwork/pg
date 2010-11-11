c81a349dd59c39afbf6ceb988e9523c6adaced0c
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2010-11-11 13:03:14 +0000

    transferred some much used macro files to pg/macros for those not including the NPL/macros/Union  directory in their macros search path
    added PGanalyzeGraph to support graphical input
    
    added POD documenta^tion to LiveGraphics3D.pl
    
    
    
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@6499 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ macros/PGanalyzeGraph.pl
@@ -0,0 +1,98 @@
+sub _PGanalyzeGraph_init {}
+
+################################################################
+# subroutines
+################################################################
+
+
+
+=head4  detect_intervals
+
+   input: $pointDisplayString 
+          return delimited string of triplets 
+          each triplet has the form  x y yp  (space delimited)
+          giving the x, y and y'(x) values at x respectively
+   return: (\@combined_intervals, \@values)
+          @values contains the y values of the function in order
+          @combined_intervals contains anonymous arrays of the form
+               [ $slope, $left_x, $right_x]  indicating the gradient on that segment.
+               successive intervals will have different slopes.
+               
+               
+=cut
+  
+sub detect_intervals {
+	my $pointDisplayString = shift;
+	my @intervals;
+	my @combined_intervals=();
+	my @points;
+	my @values;
+	$out_string ='';
+	return "" unless defined $pointDisplayString and $pointDisplayString =~/\S/;
+	@pointDisplayLines = split("\n",$pointDisplayString);
+	#drop first line
+	#shift @pointDisplayLines;
+	my ($prev_x, $prev_y, $prev_yp) = (undef);
+	my $slope;
+	
+    #first calculate the average gradient on each interval
+    
+	foreach my $line (@pointDisplayLines) {
+	    chomp($line);
+	    next unless $line =~/\S/;  # skip blank lines
+	    ($x,$y,$yp) = split(/\s+/, $line);
+
+	    if (defined $prev_x) {
+	    	 $slope = $y - $prev_y;
+			
+			if ($slope >0) {
+				$slope_str="increasing";
+			} elsif ($slope <0) {
+				$slope_str="decreasing";
+			} else {
+				$slope_str = "constant";
+			}
+			push @intervals, [$slope_str, $prev_x, $x];  
+			
+			#TEXT("f is $slope_str on the interval [$prev_x, $x]$BR");
+		}
+		#TEXT("x=$x y = $y yp = $yp $BR");
+		push @points, [$x, $y, $yp];
+		push @values, $y; 
+		$prev_x =$x; $prev_y=$y; $prev_yp = $yp;
+		
+	}
+	my $prev_slope = undef;
+	my ($left_x, $right_x);
+	
+	########
+	# Combine adjacent intervals with the same properites
+	########
+	foreach my $item (@intervals) {
+		if (defined $prev_slope) {
+			if ($prev_slope eq $item->[0]) {
+				$right_x = $item->[2];			
+			} else {
+				push @combined_intervals, [$prev_slope, $left_x, $right_x];
+				$left_x = $item->[1];
+				$right_x = $item->[2];
+			}
+		
+		} else {
+			$left_x = $item->[1];
+			$right_x = $item->[2];
+		}
+		$prev_slope = $item->[0]; 
+		# warn "intervals",join(" ", @combined_intervals);
+	}
+	push @combined_intervals, [$prev_slope, $left_x, $right_x];
+	
+  (\@combined_intervals, \@values);
+}
+
+
+
+
+
+
+1;
