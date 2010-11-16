3dba0281b1c6f39b92476ba4c06a966722ae560f
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2010-11-16 02:22:05 +0000

    second attempt to cure defined(%$type) without triggering the new perl compiler warning that checking the existence of hashes is deprecated.   now just using %$type.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/pg@6534 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/Value/AnswerChecker.pm
+++ lib/Value/AnswerChecker.pm
@@ -1593,9 +1593,9 @@ sub cmp_defaults {
     &{$type.'cmp_defaults'}($self,@_),
     upToConstant => 0,
     showDomainErrors => 1,
-#  ) if defined(%$type) && $self->type ne 'List';
-	) if ( ref($type)=~/HASH/ ) && $self->type ne 'List';
-
+#  ) if defined(%$type) && $self->type ne 'List'; #causes errors in newest perl
+#	) if ( ref($type)=~/HASH/ ) && $self->type ne 'List'; # does NOT work --causes FortLewis error
+   ) if %$type && $self->type ne 'List';   
   my $element;
   if ($self->{tree}->class eq 'List') {$element = $self->Package("Formula")->new($self->{tree}{coords}[0])}
     else {$element = $self->Package("Formula")->new(($self->createRandomPoints(1))[1]->[0]{data}[0])}
