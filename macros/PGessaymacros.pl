44bb9b78a19cf63348090a35e30698ae8cdb412d
Michael Gage <gage@math.rochester.edu>
2013-06-08 11:05:57 -0400

    Protect HTML::Scrubber from undefined arguments

--- macros/PGessaymacros.pl
+++ macros/PGessaymacros.pl
@@ -64,7 +64,9 @@ sub essay_cmp {
 	    comment => 0
 	    );
 	
-	$student->{original_student_ans} = $scrubber->scrub($student->{original_student_ans});
+	$student->{original_student_ans} = $scrubber->scrub(
+		(defined $student->{original_student_ans})? $student->{original_student_ans} :'';
+	);
 
 	# always returns false but stuff should check for the essay flag and avoid the red highlighting
 	loadMacros("contextTypeset.pl");
