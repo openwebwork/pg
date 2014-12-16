157774b77a1d225bdd00ceabcf4fbfb881f436c1
Geoff Goehle <goehle@gmail.com>
2014-12-16 15:31:44 -0500

    Made some changes to help with accessibility

--- macros/parserPopUp.pl
+++ macros/parserPopUp.pl
@@ -112,7 +112,7 @@ sub MENU {
   my $answer_value = (defined($main::inputs_ref->{$name}) ? $main::inputs_ref->{$name} : '');
   my $label = main::generate_aria_label($name);
   if ($main::displayMode =~ m/^HTML/) {
-    $menu = qq!<SELECT NAME="$name" id="$name" aria-label="$label" SIZE=1>\n!;
+    $menu = qq!<SELECT class="pg-select" NAME="$name" id="$name" aria-label="$label" SIZE=1>\n!;
     foreach my $option (@$list) {
       my $selected = ($option eq $answer_value) ? " SELECTED" : "";
       $menu .= qq!<OPTION$selected VALUE="$option">$option</OPTION>\n!;
