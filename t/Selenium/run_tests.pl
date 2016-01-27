920870379128099683242c6071007d726f750829
Geoff Goehle <goehle@gmail.com>
2016-01-26 21:56:00 -0500

    Initial commit for Selenium testing harness.

---
+++ t/Selenium/run_tests.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+
+################################################################################
+# WeBWorK Online Homework Delivery System
+# Copyright © 2000-2007 The WeBWorK Project, http://openwebwork.sf.net/
+# $CVSHeader: webwork2/lib/WeBWorK.pm,v 1.104 2010/05/15 18:44:26 gage Exp $
+# 
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of either: (a) the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version, or (b) the "Artistic License" which comes with this package.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
+# Artistic License for more details.
+################################################################################
+
+# This runs all of the test files passed via the command line.  If nothing is
+# passed via the command line it runs all of the files in
+# pg/t/Selenium/Tests
+
+# All tests in this test stuite should be run on a fresh webwork install
+# with no courses (besides the admin course) and an admin course user with
+# user name admin and password admin.  (Basically the setup you get from
+# running the webwork installer).
+
+
+use strict; 
+use warnings;
+
+BEGIN{ die('You need to set the WEBWORK_ROOT environment variable.\n')
+	   unless($ENV{WEBWORK_ROOT});}
+use lib "$ENV{WEBWORK_ROOT}/t";
+
+use File::Find;
+use Test::Harness;
+
+BEGIN{ 
+    my $ce = new WeBWorK::CourseEnvironment({
+	webwork_dir => $ENV{WEBWORK_ROOT},
+					 });
+    
+    my $pg_dir = $ce->{pg_dir};
+  }
+
+use constant TESTING_DIRECTORY => "$pg_dir/t/Selenium/Tests";
+
+my @files;
+
+if (length(@ARGV)) {
+  
+  @files = @ARGV;
+  
+} else {
+  
+  @files = find(sub {/*.t/;}, TESTING_DIRECTORY);
+}
+
+runtests( @test_Files);
+  
