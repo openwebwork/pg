a72087f09a5426c0d2a7e8fd0ddfe4c692b33936
Geoff Goehle <goehle@gmail.com>
2016-03-31 10:55:34 -0400

    proofreading changes.

--- lib/PGcore.pm
+++ lib/PGcore.pm
@@ -723,7 +723,7 @@ sub maketext {
   my $self = shift;
   # uncomment this to check to see if strings are run through
   # maketext.  
-  return '_'.  &{ $self->{maketext}}(@_).'_';
+  return 'xXx'.  &{ $self->{maketext}}(@_).'xXx';
   &{ $self->{maketext}}(@_);
 }
 sub includePGtext { 
