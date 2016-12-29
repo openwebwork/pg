508858cfbfd7fd7f03e79e979d588edd45423c36
Michael Gage <gage@math.rochester.edu>
2016-12-28 21:05:36 -0500

    Add ability to set the extension to png, gif or sag (etc). Remove set_command_line() subroutine — this is supposed to be replaced by referring to site.conf updated the test files in t/tikz_test

--- lib/TikZ_Image2.pm
+++ lib/TikZ_Image2.pm
@@ -28,6 +28,7 @@ sub new {
 			copy_command     => WeBWorK::PG::IO::copyCommand(),
 			# rh_envir       => $rh_envir,   # pointer to the environment
 			displayMode      => $rh_envir->{displayMode},
+			ext              => 'png',  # or svg or png or gif
 	};
 	return bless $self, $class;
 }
@@ -35,7 +36,7 @@ sub new {
 #FIXME -- passing in the actual commands to TikZ_Image2.pm
 #FIXME -- is extremely dangerous.  It gives command line access
 #FIXME -- to authors to insert just about any command.
-
+#FIXME -- allow ext to be overridden
 #typical values for command line apps
 
 
@@ -59,26 +60,26 @@ sub new {
 # externalConvert	=>	 
 
 my $extern_pdflatex='';
-sub set_commandline_mode {
-	my $self = shift;
-	my $commandline_mode = shift;    #FIXME this section is temporary
-	my $working_dir = $self->{working_dir};
-	if ($commandline_mode eq 'wwtest') {		
-		$extern_pdflatex="/Volumes/WW_test/opt/local/bin/pdflatex --shell-escape";
-		$self->{convert_command}  = "convert $working_dir/hardcopy.pdf "; #add destination file later
-		$self->{copy_command}     = "cp ";
-	} elsif ( $commandline_mode eq 'macbook') {
-		$extern_pdflatex ="/Library/TeX/texbin/pdflatex --shell-escape";
-		$self->{convert_command}  = "/usr/local/bin/convert $working_dir/hardcopy.pdf ";
-		$self->{copy_command}     = "cp ";
-	} elsif ( $commandline_mode eq 'hosted2') {
-		$extern_pdflatex="/usr/local/bin/pdflatex --shell-escape";
-		$self->{convert_command}  = "/usr/local/bin/convert $working_dir/hardcopy.pdf "; #add destination file later
-		$self->{copy_command}     = "cp ";
-	}
-	$self->{pdflatex_command} =  "cd " . $working_dir . " && "
-		. $extern_pdflatex. " >pdflatex.stdout 2>pdflatex.stderr hardcopy.tex";
-}
+# sub set_commandline_mode {
+# 	my $self = shift;
+# 	my $commandline_mode = shift;    #FIXME this section is temporary
+# 	my $working_dir = $self->{working_dir};
+# 	if ($commandline_mode eq 'wwtest') {		
+# 		$extern_pdflatex="/Volumes/WW_test/opt/local/bin/pdflatex --shell-escape";
+# 		$self->{convert_command}  = "convert $working_dir/hardcopy.pdf "; #add destination file later
+# 		$self->{copy_command}     = "cp ";
+# 	} elsif ( $commandline_mode eq 'macbook') {
+# 		$extern_pdflatex ="/Library/TeX/texbin/pdflatex --shell-escape";
+# 		$self->{convert_command}  = "/usr/local/bin/convert $working_dir/hardcopy.pdf ";
+# 		$self->{copy_command}     = "cp ";
+# 	} elsif ( $commandline_mode eq 'hosted2') {
+# 		$extern_pdflatex="/usr/local/bin/pdflatex --shell-escape";
+# 		$self->{convert_command}  = "/usr/local/bin/convert $working_dir/hardcopy.pdf "; #add destination file later
+# 		$self->{copy_command}     = "cp ";
+# 	}
+# 	$self->{pdflatex_command} =  "cd " . $working_dir . " && "
+# 		. $extern_pdflatex. " >pdflatex.stdout 2>pdflatex.stderr hardcopy.tex";
+# }
 # Insert your TikZ image code, not including begin and end tags, as a single
 # string parameter for this method. Works best single quoted.
 sub addTex {
@@ -86,7 +87,15 @@ sub addTex {
 	$self->{code} .= shift;
 }
 
+sub ext {
+	my $self = shift;
+	if (@_) {
+		return $self->{ext} = shift;
+	} else {
+		return $self->{ext};
+	}
 
+}
 sub header {
 	my $self = shift;
 	my @output=();
@@ -155,10 +164,11 @@ sub convert {
 	my $working_dir =  $self->{working_dir};
 	my $file_name = $self->{file_name};
 	my $file_path = "$working_dir/$file_name";
+	my $ext = $self->{ext};   # or png or gif
 	my $convert_command = $self->{convert_command};
-	warn "converting: ","$convert_command  $file_path.png","\n"; 
-	system "$convert_command  $file_path.png";
-	return -r "$file_path.png";
+	warn "converting: ","$convert_command  $file_path.$ext","\n"; 
+	system "$convert_command  $file_path.$ext";
+	return -r "$file_path.$ext";
 }
 
 sub clean_up {
@@ -178,11 +188,12 @@ sub copy {
 	my $file_path = "$working_dir/$file_name";
 	my $destination_path = $self->{destination_path};
 	my $copy_command = $self->{copy_command};
+	my $ext = $self->{ext};
 	if ($self->{displayMode} ne 'TeX') {
-		warn "copy: $copy_command $working_dir/$file_name.png $destination_path.png\n";	
-		system "$copy_command $working_dir/$file_name.png $destination_path.png";
-		$self->{final_destination_path}= "$destination_path.png";
-		return -r "$destination_path.png";
+		warn "copy: $copy_command $working_dir/$file_name.$ext $destination_path.$ext\n";	
+		system "$copy_command $working_dir/$file_name.$ext $destination_path.$ext";
+		$self->{final_destination_path}= "$destination_path.$ext";
+		return -r "$destination_path.$ext";
 	} else {
 		warn "copy: $copy_command $working_dir/hardcopy.pdf $destination_path.pdf\n";	
 		system "$copy_command $working_dir/hardcopy.pdf $destination_path.pdf";
