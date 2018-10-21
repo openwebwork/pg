9b5d07b8a8d36f3d1ebd2a69deedd87e43869751
Michael Gage <gage@math.rochester.edu>
2018-10-20 21:03:18 -0400

    Update copyright

--- lib/PGalias.pm
+++ lib/PGalias.pm
@@ -1,6 +1,6 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
-# Copyright © 2000-2007 The WeBWorK Project, http://openwebwork.sf.net/
+# Copyright &copy; 2000-2018 The WeBWorK Project, http://openwebwork.sf.net/
 # $CVSHeader: pg/lib/PGalias.pm,v 1.6 2010/05/15 18:41:23 gage Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
--- lib/PGanswergroup.pm
+++ lib/PGanswergroup.pm
@@ -1,6 +1,6 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
-# Copyright © 2000-2007 The WeBWorK Project, http://openwebwork.sf.net/
+# Copyright &copy; 2000-2018 The WeBWorK Project, http://openwebwork.sf.net/
 # $CVSHeader: pg/lib/PGanswergroup.pm,v 1.1 2010/05/14 11:39:02 gage Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
