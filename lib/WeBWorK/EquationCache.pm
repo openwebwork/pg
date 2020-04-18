6dda35338bbc8d4dc5bbe317fc32a72b614075f8
Michael Gage <1203580+mgage@users.noreply.github.com>
2020-04-18 15:22:03 -0400

    remove importation of unnecessary subroutines

--- lib/WeBWorK/EquationCache.pm
+++ lib/WeBWorK/EquationCache.pm
@@ -37,7 +37,7 @@ extending to the end of the line is also ignored.
 use strict;
 use warnings;
 use Digest::MD5 qw(md5_hex);
-use Encode qw(encode_utf8 decode_utf8 decode);
+use Encode qw(encode_utf8 );
 use Fcntl qw(:DEFAULT :flock);
 BEGIN { my @_junk = (O_RDWR,O_CREAT,LOCK_EX) } # get rid of "subroutine redefined" warnings
 
