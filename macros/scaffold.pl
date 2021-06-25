97ccca6c76659060f43de844ed6eaa7f290cf8a5
Alex Jordan <alex.jordan@pcc.edu>
2021-06-24 23:55:29 -0700

    adjust newlines in PTX output

--- macros/scaffold.pl
+++ macros/scaffold.pl
@@ -625,7 +625,7 @@ sub add_container {
       '<div class="accordion-inner">'
     ],
     TeX => ["\\par{\\bf $number $title}\\addtolength{\\leftskip}{15pt}\\par "],
-    PTX => $name ? ["<task>\n", "<title>$name</title>"] : ["<task>\n"],
+    PTX => $name ? ["<task>\n", "<title>$name</title>\n"] : ["<task>\n"],
   )});
   push(@$PG_OUTPUT,main::MODES(
     HTML => '</div></div></div>',
