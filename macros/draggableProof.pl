bc400dee58e8e823e2b18b100d58d4266432b166
Ping-Shun Chan <31397850+pschan-gh@users.noreply.github.com>
2021-07-23 21:25:03 +0800

    initial commit

--- macros/draggableProof.pl
+++ macros/draggableProof.pl
@@ -2,283 +2,204 @@
 # To do: display student answers and correct answers in TeX mode properly.
 # To do: put jquery.nestable.js in a universal spot on every webwork server.
 
-loadMacros("PGchoicemacros.pl");
+loadMacros("PGchoicemacros.pl",
+"MathObjects.pl",
+"levenshtein.pl",
+ );
 
 sub _draggableProof_init {
-  PG_restricted_eval("sub DraggableProof {new draggableProof(\@_)}");
-
-# post global javascript
-# using sources in cdnjs.cloudflare.com
-
-main::POST_HEADER_TEXT(main::MODES(TeX=>"", HTML=><<END_SCRIPTS));
-
-	<script src= "https://cdnjs.cloudflare.com/ajax/libs/nestable2/1.6.0/jquery.nestable.min.js">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nestable2/1.6.0/jquery.nestable.min.css">
-	</script>
-
-    <script>
-      function _nestableUpdate (e) {
-          var dd = e.length ? e : \$(e.target);
-          var ans = dd["0"].getAttribute("data-ans");
-          var li_tags = dd["0"].getElementsByTagName("li");
-          var list = [];
-          for (var i = 0, n = li_tags.length; i < n; i++) {
-              list.push(li_tags[i].id);
-          }
-          \$("#"+ans).val(list.join(","));
-      }
-    </script>
-END_SCRIPTS
-} # end _draggableProof_init
+    ADD_CSS_FILE("https://cdnjs.cloudflare.com/ajax/libs/nestable2/1.6.0/jquery.nestable.min.css", 1);
+    ADD_JS_FILE("https://cdnjs.cloudflare.com/ajax/libs/nestable2/1.6.0/jquery.nestable.min.js", 1);
+    ADD_CSS_FILE("js/apps/DragNDrop/dragndrop.css", 0);
+    ADD_JS_FILE("js/apps/DragNDrop/dragndrop.js", 0, { defer => undef });
+    PG_restricted_eval("sub DraggableProof {new draggableProof(\@_)}");
+}
 
 package draggableProof;
 
-my $n = 0;  # number of nestable lists so far
-
 sub new {
-  $n++;
-  my $self = shift; my $class = ref($self) || $self;
-  my $proof = shift || []; my $extra = shift || [];
-  my %options = (
-    SourceLabel => "Choose from these sentences:",
-    TargetLabel => "Your Proof:",
-    id => "P$n",
-    @_
-  );
-  my $lines = [@$proof,@$extra];
-  my $numNeeded = scalar(@$proof);
-  my $numProvided = scalar(@$lines);    
-  my @order = main::shuffle($numProvided);
-  my @unorder = main::invert(@order);
-  $self = bless {
-    lines => $lines,
-    numNeeded => $numNeeded, numProvided => $numProvided,
-    order => \@order, unordered => \@unorder,
-    proof => "$options{id}-".join(",$options{id}-",@unorder[0..$numNeeded-1]),
-    %options
-  }, $class;
-  $self->AnswerRule;
-  $self->ScriptAndStyles;
-  $self->GetAnswer;
-  return $self;
-}
+	my $self = shift; 
+	my $class = ref($self) || $self;
+	
+	my $proof = shift || []; 
+	my $extra = shift || [];	
+	my %options = (
+		SourceLabel => "Choose from these sentences:",
+		TargetLabel => "Your Proof:",
+		NumBuckets => 2,
+		Levenshtein => 0,
+		@_
+	);
+	
+	my $lines = [@$proof,@$extra];	
+	my $numNeeded = scalar(@$proof);
+	my $numProvided = scalar(@$lines);
+	my @order = main::shuffle($numProvided);
+	my @unorder = main::invert(@order);
 
-sub lines {my $self = shift; return @{$self->{lines}}}
-sub numNeeded {(shift)->{numNeeded}}
-sub numProvided {(shift)->{numProvided}}
-sub order {my $self = shift; return @{$self->{order}}}
-sub unorder {my $self = shift; return @{$self->{unorder}}}
-
-# In principle, the styles (for example, color and size of the tiles)
-# could be customized for each draggableProof instance. This is why
-# each instance receives its own CSS container class.
-
-sub ScriptAndStyles {
-  my $self = shift; my $id = $self->{id};
-  
-  main::POST_HEADER_TEXT(main::MODES(TeX=>"", HTML=><<"  SCRIPT_AND_STYLE"));
-    <script type="text/javascript">
-      \$(document).ready(function() {
-          \$("#nestable-$id-source").nestable({
-              group: "$id",
-              maxDepth: 1,
-              scroll: true,
-              callback: _nestableUpdate
-          });
-          \$("#nestable-$id-source").on('lostItem', _nestableUpdate);
-          \$("#nestable-$id-target").nestable({
-              group: "$id",
-              maxDepth: 1,
-              scroll: true,
-              callback: _nestableUpdate
-          });
-          \$("#nestable-$id-target").on('lostItem', _nestableUpdate);
-      });
-    </script>
-
-    <style type="text/css">
-    .nestable-$id-container {
-        width: 350px;
-        float: left;
-        margin: 10px;
-        padding: 0;
-        color: #000000;
-        border:1 px solid #388E8E;
-        text-align: center;
-    }
-    .nestable-label {
-        margin: 10px 0 10px 0;
-    }
-    .dd, .dd-list, .dd-item {
-        display: block;
-        position: relative;
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        min-height: 30px;
-    }
-    .dd-empty, .dd-handle, .dd-placeholder {
-        display: block;
-        position: relative;
-        margin: 0 10px 10px 10px;
-        padding: 4px;
-        min-height: 30px;
-        -moz-box-sizing: border-box;
-        box-sizing: border-box;
-    }   
-    .dd-handle {
-        background: #F5DEB3;
-        border: 1px solid #388E8E;
-        -webkit-border-radius: 5px;
-        border-radius: 5px;
-        text-align: center;
-    }
-    .dd-handle:hover {
-        cursor: pointer;
-    }
-    .dd-placeholder {
-        background: #f2fbff;
-        border: 1px dashed #b6bcbf;
-        -webkit-border-radius: 5px;
-        border-radius: 5px;
-    }
-    .dd-dragel {
-        position: absolute;
-        pointer-events: none;
-        z-index: 9999;
-    }
-    .dd-dragel > .dd-item .dd-handle {
-        margin-top: 0;
-    }
-    .dd-dragel .dd-handle {
-        -webkit-box-shadow: 2px 4px 6px 0 rgba(0,0,0,.1);
-        box-shadow: 2px 4px 6px 0 rgba(0,0,0,.1);
-        opacity: 0.8;
-    }
-    </style>
-  SCRIPT_AND_STYLE
-}
-
-sub AnswerRule {
-  my $self = shift;
-  my $rule = main::ans_rule(1);
-  $self->{tgtAns} = ""; $self->{tgtAns} = $1 if $rule =~ m/id="(.*?)"/;
-  $self->{srcAns} = $self->{tgtAns}."-src";
-  main::RECORD_FORM_LABEL($self->{srcAns}); # use this for release 2.13 and comment out for develop
-  #$main::PG->store_persistent_data;  # uncomment this for develop and releases beyond 2.13
-  my $ext = main::NAMED_ANS_RULE_EXTENSION($self->{srcAns},1,answer_group_name=>$self->{srcAns}.'-src');
-  main::TEXT( main::MODES(TeX=>"", HTML=>'<div style="display:none" id="Proof">'.$rule.$ext.'</div>'));
-}
-
-sub GetAnswer {
-  my $self = shift; my $previous;
-
-  # Retrieve the previous state of the right column.
-  $previous = $main::inputs_ref->{$self->{tgtAns}} || "";
-  $previous =~ s/$self->{id}-//g; $self->{previousTarget} = [split(/,/,$previous)];
-
-  # Calculate the complement of the right column.
-  my %prevTarget = map {$_ => 1} @{$self->{previousTarget}};
-  my @diff = grep {not $prevTarget{$_}} (0..$self->{numProvided}-1);
-
-  # If the previous state of the left column has been saved, use it. (This ensures that the tiles 
-  # in the left column are kept in the same order that the user had arranged them). If it has not
-  # been saved, use the complement of the right column.
-  $previous = $main::inputs_ref->{$self->{srcAns}} || "$self->{id}-".join(",$self->{id}-",@diff);
-  $previous =~ s/$self->{id}-//g; $self->{previousSource} = [split(/,/,$previous)];
+	my $shuffled_lines = [ map {$lines->[$_]} @order ];
+	
+	my $answer_input_id = main::NEW_ANS_NAME() unless $self->{answer_input_id};
+	my $ans_rule = main::NAMED_HIDDEN_ANS_RULE($answer_input_id);
+	# warn main::pretty_print $ans_rule;
+	
+	my $dnd;
+	if ($options{NumBuckets} == 2) {
+		$dnd = new DragNDrop($answer_input_id, $shuffled_lines, [{indices=>[0..$numProvided-1], label=>$options{'SourceLabel'}}, {indices=>[], label=>$options{'TargetLabel'}}], AllowNewBuckets => 0);
+	} elsif($options{NumBuckets} == 1) {
+		$dnd = new DragNDrop($answer_input_id, $shuffled_lines, [{indices=>[0..$numProvided-1], label=>$options{'TargetLabel'}}], AllowNewBuckets => 0);
+	}
+	
+	my $proof = $options{NumBuckets} == 2 ? 
+	main::List(main::List(@unorder[$numNeeded .. $numProvided - 1]), main::List(@unorder[0..$numNeeded-1]))
+	: main::List('('.join(',', @unorder[0..$numNeeded-1]).')');
+		
+		
+	$self = bless {
+		lines => $lines,
+		shuffled_lines => $shuffled_lines,
+		numNeeded => $numNeeded, 
+		numProvided => $numProvided,
+		order => \@order, 
+		unorder => \@unorder,
+		proof => $proof,
+		answer_input_id => $answer_input_id,
+		dnd => $dnd,
+		ans_rule => $ans_rule,
+		%options,
+	}, $class;
+	
+	my $previous = $main::inputs_ref->{$answer_input_id} || '';
+	
+	if ($previous eq "") {
+		if ($self->{NumBuckets} == 2) {
+			$dnd->addBucket([0..$numProvided-1], $options{'SourceLabel'});
+			$dnd->addBucket([], $options{'TargetLabel'});
+		} elsif ($self->{NumBuckets} == 1) {
+			$dnd->addBucket([0..$numProvided-1], $options{'TargetLabel'});
+		}
+	} else {
+		my @matches = ( $previous =~ /(\(\d*(?:,\d+)*\))+/g );
+		if ($self->{NumBuckets} == 2) {
+			my $indices1 = [ split(',', @matches[0] =~ s/\(|\)//gr) ];		
+			$dnd->addBucket($indices1, $options{'SourceLabel'});		
+			my $indices2 = [ split(',', @matches[1] =~ s/\(|\)//gr) ];
+			$dnd->addBucket($indices2, $options{'TargetLabel'});
+		} else {
+			my $indices1 = [ split(',', @matches[0] =~ s/\(|\)//gr) ];
+			$dnd->addBucket($indices1, $options{'TargetLabel'});
+		}
+	}
+		
+	return $self;
 }
 
 sub Print {
-  my $self = shift;
+	my $self = shift;
 
-  if ($main::displayMode ne "TeX") { # HTML mode
-
-    return join("\n",
-       '<div style="min-width:750px;">',
-          $self->Source,
-          $self->Target,
-       '<br clear="all" />',
-      '</div>',
-    );
-
-  } else { # TeX mode
-
-    return join("\n",
-          $self->Source,
-          $self->Target,
-    );
-
-  }
-
-}
-
-sub Source {
-  my $self = shift;
-  return $self->Bucket("source",$self->{srcAns},$self->{SourceLabel},$self->{previousSource});
-}
-
-sub Target {
-  my $self = shift;
-  return $self->Bucket("target",$self->{tgtAns},$self->{TargetLabel},$self->{previousTarget});
-}
-
-sub Bucket {
-  my $self = shift; my $id = $self->{id};
-  my ($name,$ans,$label,$previous) = @_;
-
-  if ($main::displayMode ne "TeX") { # HTML mode
-
-    my @lines = ();
-    push(@lines, '<div class="nestable-'.$id.'-container">',
-      '<div class="nestable-label">'.$label.'</div>',
-      '<div class="dd" id="nestable-'.$id.'-'.$name.'" data-ans="'.$ans.'">'
-    );
-    if (scalar @{$previous} > 0) {
-      push(@lines, '<ol class="dd-list">');
-      foreach my $i (@{$previous}) {
-        push(@lines, '<li class="dd-item" id="'.$id.'-'.$i.'"><div class="dd-handle">'.$self->{lines}[$self->{order}[$i]].'</div></li>');
-      }
-      push(@lines, '</ol>');
-    }
-    push(@lines, 
-      '</div>',
-      '</div>'
-    );
-    return join("\n",@lines);
-
-  } else { # TeX mode
-
-    if (@{$previous}) { # array is nonempty
-      my @lines = ('\\begin{itemize}');
-      foreach my $i (@{$previous}) {
-        push(@lines,'\\item '.$self->{lines}[$self->{order}[$i]] )
-      }
-      push(@lines,'\\end{itemize}');
-      return join("\n",@lines);
-    } else {
-      return '';
-    }
-  }
-
+	if ($main::displayMode eq 'TeX') {
+		return "\\begin{itemize}\\item".join("\n\n\\item\n" , @{ $self->{aggregate_list} })."\\end{itemize}";
+	}
+	
+	my $html = $self->{dnd}->toHTML;
+	my $ans_rule = $self->{ans_rule};
+	
+	if ($main::displayMode ne "TeX") { # HTML mode
+		return join("\n",
+			'<div style="min-width:750px;">',
+			$ans_rule,
+			$html,
+			'<br clear="all" />',
+			'</div>',
+		);
+	} else { # TeX mode
+		return join("\n",
+			$ans_rule,
+			$html,
+		);
+	}
 }
 
 sub cmp {
-  my $self = shift;
-  return main::str_cmp($self->{proof})->withPreFilter("erase")->withPostFilter(sub {$self->filter(@_)});
+	my $self = shift;	
+	if ($self->{Levenshtein} == 0) {
+		return $self->{proof}->cmp(ordered => 1, removeParens => 1)->withPreFilter("erase")->withPostFilter(sub {$self->filter(@_)});
+	} else {
+		return $self->{proof}->cmp(ordered => 1, removeParens => 1)->withPreFilter("erase")->withPostFilter(sub {$self->levenshtein_filter(@_)});
+	}
 }
 
 sub filter {
-  my $self = shift; my $ans = shift;
-  my @line = $self->lines; my @order = $self->order;
-  my $correct = $ans->{correct_ans}; $correct =~ s/$self->{id}-//g;
-  my $student = $ans->{student_ans}; $student =~ s/$self->{id}-//g;
-  my @correct = @line[map {@order[$_]} split(/,/,$correct)];
-  my @student = @line[map {@order[$_]} split(/,/,$student)];
-  $ans->{preview_latex_string} = "\\begin{array}{l}\\text{".join("}\\\\\\text{",@student)."}\\end{array}";
-  $ans->{student_ans} = "(see preview)";
-  $ans->{correct_ans_latex_string} = "\\begin{array}{l}\\text{".join("}\\\\\\text{",@correct)."}\\end{array}";
-  $ans->{correct_ans} = join("<br />",@correct);
-  return $ans;
+	my $self = shift; my $anshash = shift;
+		
+	my @lines = @{$self->{lines}}; 
+	my @order = @{$self->{order}};
+	
+	my $actual_answer = $anshash->{student_ans} =~ s/\(|\)|\s*//gr;
+	my $correct = $anshash->{correct_ans} =~ s/\(|\)|\s*//gr;
+	
+	if ($self->{NumBuckets} == 2) {
+		my @matches = ( $anshash->{student_ans} =~ /(\(\d*(?:,\s*\d+)*\)|\d+)/g );
+		$actual_answer = @matches == 2 ? $matches[1] =~ s/\(|\)|\s*//gr : '';
+		
+		@matches = ( $anshash->{correct_ans} =~ /(\(\d*(?:,\s*\d+)*\)|\d+)/g );
+		$correct = @matches == 2 ? $matches[1] =~ s/\(|\)|\s*//gr : '';
+		
+		$anshash->{correct_ans} = main::List($correct); # change to main::Set if order does not matter
+		$anshash->{student_ans} = main::List($actual_answer); # change to main::Set if order does not matter
+		$anshash->{original_student_ans} = $anshash->{student_ans};
+		$anshash->{student_value} = $anshash->{student_ans};
+		$anshash->{student_formula} = $anshash->{student_ans};
+		
+		if ($anshash->{correct_ans} eq $anshash->{student_ans}) {
+			$anshash->{score} = 1;
+		}
+	}
+	
+	my @correct = @lines[map {@order[$_]} split(/,/, $correct)];
+	my @student = @lines[map {@order[$_]} split(',', $actual_answer)];
+	 
+	$anshash->{student_ans} = "(see preview)";
+	$anshash->{correct_ans_latex_string} = "\\begin{array}{l}\\text{".join("}\\\\\\text{",@correct)."}\\end{array}";
+	$anshash->{correct_ans} = join("<br />",@correct);
+	$anshash->{preview_latex_string} = "\\begin{array}{l}\\text{".join("}\\\\\\text{",@student)."}\\end{array}";
+	
+	return $anshash;
 }
 
+sub levenshtein_filter {
+	my $self = shift; my $anshash = shift;
+		
+	my @lines = @{$self->{lines}}; 
+	my @order = @{$self->{order}};
+	
+	my $actual_answer = $anshash->{student_ans} =~ s/\(|\)|\s*//gr;
+	my $correct = $anshash->{correct_ans} =~ s/\(|\)|\s*//gr;
+	
+	if ($self->{NumBuckets} == 2) {
+		my @matches = ( $anshash->{student_ans} =~ /(\(\d*(?:,\s*\d+)*\)|\d+)/g );
+		$actual_answer = @matches == 2 ? $matches[1] =~ s/\(|\)|\s*//gr : '';
+		
+		@matches = ( $anshash->{correct_ans} =~ /(\(\d*(?:,\s*\d+)*\)|\d+)/g );
+		$correct = @matches == 2 ? $matches[1] =~ s/\(|\)|\s*//gr : '';
+		
+		$anshash->{correct_ans} = main::List($correct); # change to main::Set if order does not matter
+		$anshash->{student_ans} = main::List($actual_answer); # change to main::Set if order does not matter
+		$anshash->{original_student_ans} = $anshash->{student_ans};
+		$anshash->{student_value} = $anshash->{student_ans};
+		$anshash->{student_formula} = $anshash->{student_ans};
+		
+	}
+	
+	$anshash->{score} = 1 - levenshtein::levenshtein($correct, $actual_answer, ',');
+	 
+	my @correct = @lines[map {@order[$_]} split(/,/, $correct)];
+	my @student = @lines[map {@order[$_]} split(',', $actual_answer)];
+	$anshash->{student_ans} = "(see preview)";
+	$anshash->{correct_ans_latex_string} = "\\begin{array}{l}\\text{".join("}\\\\\\text{",@correct)."}\\end{array}";
+	$anshash->{correct_ans} = join("<br />",@correct);
+	$anshash->{preview_latex_string} = "\\begin{array}{l}\\text{".join("}\\\\\\text{",@student)."}\\end{array}";
+	
+	return $anshash;
+}
 1;
---
+++ macros/draggableSubsets.pl
@@ -0,0 +1,139 @@
+# Done: show possible choices in TeX mode
+# To do: display student answers and correct answers in TeX mode properly.
+# To do: put jquery.nestable.js in a universal spot on every webwork server.
+
+loadMacros(
+"PGchoicemacros.pl",
+"MathObjects.pl",
+);
+
+sub _draggableSubsets_init {
+	ADD_CSS_FILE("https://cdnjs.cloudflare.com/ajax/libs/nestable2/1.6.0/jquery.nestable.min.css", 1);
+	ADD_JS_FILE("https://cdnjs.cloudflare.com/ajax/libs/nestable2/1.6.0/jquery.nestable.min.js", 1);
+	ADD_CSS_FILE("js/apps/DragNDrop/dragndrop.css", 0);
+	ADD_JS_FILE("js/apps/DragNDrop/dragndrop.js", 0, { defer => undef });
+	PG_restricted_eval("sub DraggableSubsets {new draggableSubsets(\@_)}");
+}
+
+package draggableSubsets;
+
+sub new {
+	my $self = shift; 
+	my $class = ref($self) || $self;
+	
+	# user arguments
+	my $set = shift || []; 
+	my $cosets = shift || []; 
+	my $default_buckets = shift || [];
+	# end user arguments
+	
+	my $numProvided = scalar(@$set);
+	my @order = main::shuffle($numProvided);
+	my @unorder = main::invert(@order);
+
+	my $shuffled_set = [ map {$set->[$_]} @order ];
+	
+	my $default_shuffled_buckets = [];
+	if (@$default_buckets) {
+		for my $default_bucket (@$default_buckets) {
+			my $shuffled_indices = [ map {$unorder[$_]} @{ $default_bucket->{indices} } ];
+			my $default_shuffled_bucket = { 
+				label => $default_bucket->{label}, 
+				indices => $shuffled_indices,
+				removable => $default_bucket->{removable},
+			};
+			push(@$default_shuffled_buckets, $default_shuffled_bucket);			
+		} 
+	} else {
+		push(@$default_shuffled_buckets, [ { 
+			label => '', 
+			indices => [ 0..$numProvided-1 ]
+		} ]);
+	}
+		
+	my $answer_input_id = main::NEW_ANS_NAME() unless $self->{answer_input_id};	
+	my $ans_rule = main::NAMED_HIDDEN_ANS_RULE($answer_input_id);
+	my $dnd = new DragNDrop($answer_input_id, $shuffled_set, $default_shuffled_buckets, AllowNewBuckets => 1);	
+	
+	my $previous = $main::inputs_ref->{$answer_input_id} || '';
+	
+	if ($previous eq '') {
+		for my $default_bucket ( @$default_shuffled_buckets ) {
+			$dnd->addBucket($default_bucket->{indices}, $default_bucket->{label});
+		}
+	} else {
+		my @matches = ( $previous =~ /(\(\d*(?:,\d+)*\))+/g );
+		for(my $i = 0; $i < @matches; $i++) {
+			my $match = @matches[$i] =~ s/\(|\)//gr;			
+			my $indices = [ split(',', $match) ];
+			my $label = $i < @$default_shuffled_buckets ? $default_shuffled_buckets->[$i]->{label} : '';
+			my $removable = $i < @$default_shuffled_buckets ? $default_shuffled_buckets->[$i]->{removable} : 1;
+			$dnd->addBucket($indices, $label, removable => $removable);
+		}
+	}	
+		
+	my @shuffled_cosets_array = ();
+	for my $coset ( @$cosets ) {
+		my @shuffled_coset = map {$unorder[$_]} @$coset;
+		push(@shuffled_cosets_array, main::Set(join(',', @shuffled_coset)));
+	}	
+	my $shuffled_cosets = main::List(@shuffled_cosets_array);
+			
+	
+	$self = bless {
+		set => $set,
+		shuffled_set => $shuffled_set,
+		numProvided => $numProvided,
+		order => \@order, 
+		unordered => \@unorder,
+		shuffled_cosets => $shuffled_cosets,
+		answer_input_id => $answer_input_id,
+		dnd => $dnd,
+		ans_rule => $ans_rule,
+	}, $class;
+	
+	return $self;
+}
+
+sub Print {
+	my $self = shift;
+	
+	my $html = $self->{dnd}->toHTML;
+	my $ans_rule = $self->{ans_rule};
+	
+	if ($main::displayMode ne "TeX") { # HTML mode
+		return join("\n",
+			'<div style="min-width:750px;">',
+			$ans_rule,
+			$html,
+			'<br clear="all" />',
+			'</div>',
+		);
+	} else { # TeX mode
+		return join("\n",
+			$ans_rule,
+			$html,
+		);
+	}
+}
+
+sub cmp {
+	my $self = shift;	
+	return $self->{shuffled_cosets}->cmp(ordered => 0, removeParens => 1)->withPreFilter(sub {$self->prefilter(@_)});
+}
+
+sub prefilter {
+	my $self = shift; my $anshash = shift;	
+	
+	my @student = ( $anshash->{original_student_ans} =~ /(\(\d*(?:,\s*\d+)*\)|\d+)/g );
+	
+	my @student_ans_array;
+	for my $match ( @student ) {
+		push(@student_ans_array, main::Set($match =~ s/\(|\)//gr));
+	}
+	
+	$anshash->{student_ans} = main::List(@student_ans_array);
+	
+	return $anshash;
+}
+1;
