3437f7a4a98a98367356bf7f842ff0259a48849a
pschan-gh <31397850+pschan-gh@users.noreply.github.com>
2021-09-03 18:26:09 +0800

    Formatting and white-space cleanup.
    Also, a previous update broke scoring-by-edit-distance. The variable $actualAnswer in draggableProof.pl is redefined to address this.

--- lib/DragNDrop.pm
+++ lib/DragNDrop.pm
@@ -1,16 +1,35 @@
+################################################################################
+# WeBWorK Online Homework Delivery System
+# Copyright &copy; 2000-2021 The WeBWorK Project, http://openwebwork.sf.net/
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of either: (a) the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version, or (b) the "Artistic License" which comes with this package.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
+# Artistic License for more details.
+################################################################################
+
 =head1 NAME
+
 DragNDrop.pm - Drag-N-Drop Module
-  
+
 =head1 DESCRIPTION
-DragNDrop.pm is a backend Perl module which facilitates the implementation of 
+
+DragNDrop.pm is a backend Perl module which facilitates the implementation of
 'Drag-And-Drop' in WeBWorK problems. It is meant to be used by other perl macros
 such as draggableProof.pl and draggableSubsets.pl
 
 =head1 TERMINOLOGY
+
 An HTML element into or out of which other elements may be dragged will be called a "bucket".
 An HTML element which houses a collection of buckets will be called a "bucket pool".
 
 =head1 USAGE
+
 Each macro aiming to implement drag-n-drop features must call at its initialization:
 ADD_CSS_FILE("https://cdnjs.cloudflare.com/ajax/libs/nestable2/1.6.0/jquery.nestable.min.css", 1);
 ADD_JS_FILE("https://cdnjs.cloudflare.com/ajax/libs/nestable2/1.6.0/jquery.nestable.min.js", 1);
@@ -36,15 +55,18 @@ allows student to create new buckets by clicking on a button.
 To add a bucket to an existing pool $bucket_pool, do:
 $bucket_pool->addBucket($indices);
 
-$indices is the reference to the array of indices corresponding to the statements in $aggregateList to be pre-included in the bucket. 
-For example, if the $aggregateList is ['Socrates is a man', 'all men are mortal', 'therefore Socrates is mortal'], and the bucket consists of  { 'Socrates is a man', 'therefore Socrates is mortal' }, then $indices = [0, 2].
+$indices is the reference to the array of indices corresponding to the statements in $aggregateList
+to be pre-included in the bucket.
+For example, if the $aggregateList is ['Socrates is a man', 'all men are mortal', 'therefore Socrates is mortal'],
+and the bucket consists of  { 'Socrates is a man', 'therefore Socrates is mortal' }, then $indices = [0, 2].
 
 An empty array reference, e.g. $bucket_pool->addBucket([]), gives an empty bucket.
 
 ###############################################################################
 OPTIONAL: $bucket_pool->addBucket($indices, label => 'Barrel', removable => 1)
 puts the label 'Barrel' at the top of the bucket.
-With the removable option set to 1, the bucket may be removed by the student via the click of a "Remove" button at the bottom of the bucket.
+With the removable option set to 1, the bucket may be removed by the student via the click of a "Remove" button
+at the bottom of the bucket.
 (The first created bucket may never be removed.)
 ###############################################################################
 
@@ -55,8 +77,11 @@ To output the bucket pool to LaTeX, call:
 $bucket_pool->TeX
 
 =head1 EXAMPLES
+
 See draggableProof.pl and draggableSubsets.pl
+
 =cut
+
 ###############################################################################
 
 use strict;
@@ -64,100 +89,108 @@ use warnings;
 
 package DragNDrop;
 
-sub new {    
-	my $self = shift; 
-    my $class = ref($self) || $self;
-    
-    my $answerInputId = shift; # 'id' of html <input> tag corresponding to the answer blank. Must be unique to each pool of DragNDrop buckets
-    my $aggregateList = shift; # array of all statements provided
-    my $defaultBuckets = shift; # instructor-provided default buckets with pre-included statements encoded by the array of corresponding statement indices
-    my %options = (
-		AllowNewBuckets => 0,
-		@_
+sub new {
+	my $self = shift;
+	my $class = ref($self) || $self;
+	
+	# 'id' of html <input> tag corresponding to the answer blank. Must be unique to each pool of DragNDrop buckets
+	my $answerInputId = shift;
+	
+	# array of all statements provided
+	my $aggregateList = shift;
+	
+	# instructor-provided default buckets with pre-included statements encoded
+	# by the array of corresponding statement indices
+	my $defaultBuckets = shift;
+	
+	my %options = (
+	AllowNewBuckets => 0,
+	@_
 	);
-
-    $self = bless {        
-        answerInputId => $answerInputId,        
-        bucketList => [],
-        aggregateList => $aggregateList,
-        defaultBuckets => $defaultBuckets,
+	
+	$self = bless {
+		answerInputId => $answerInputId,
+		bucketList => [],
+		aggregateList => $aggregateList,
+		defaultBuckets => $defaultBuckets,
 		%options,
-    }, $class;
-            	
-    return $self;
+	}, $class;
+	
+	return $self;
 }
 
-sub addBucket {    
-    my $self = shift; 
-    
-    my $indices = shift;
+sub addBucket {
+	my $self = shift;
+	
+	my $indices = shift;
 	
 	my %options = (
 	label => "",
 	removable => 0,
 	@_
-    );
+	);
 	
 	my $bucket = {
-        indices => $indices,
-        list => [ map { $self->{aggregateList}->[$_] } @$indices ],
-        bucket_id => scalar @{ $self->{bucketList} },
+		indices => $indices,
+		list => [ map { $self->{aggregateList}->[$_] } @$indices ],
+		bucket_id => scalar @{ $self->{bucketList} },
 		label => $options{label},
-        removable => $options{removable},
-    };
-    push(@{$self->{bucketList}}, $bucket);
-    
+		removable => $options{removable},
+	};
+	push(@{$self->{bucketList}}, $bucket);
+	
 }
 
 sub HTML {
-    my $self = shift;
-    	
-    my $out = '';
-    $out .= "<div class='bucket_pool' data-ans='$self->{answerInputId}'>";
-        
-    # buckets from instructor-defined default settings
-    for (my $i = 0; $i < @{$self->{defaultBuckets}}; $i++) {
-        my $defaultBucket = $self->{defaultBuckets}->[$i];
-        $out .= "<div class='hidden default bucket' data-bucket-id='$i' data-removable='$defaultBucket->{removable}'>";
-        $out .= "<div class='label'>$defaultBucket->{label}</div>"; 
-        $out .= "<ol class='answer'>";
-        for my $j ( @{$defaultBucket->{indices}} ) {
-            $out .= "<li data-shuffled-index='$j'>$self->{aggregateList}->[$j]</li>";
-        }
-        $out .= "</ol></div>";
-    }
-    
+	my $self = shift;
+	
+	my $out = '';
+	$out .= "<div class='bucket_pool' data-ans='$self->{answerInputId}'>";
+	
+	# buckets from instructor-defined default settings
+	for (my $i = 0; $i < @{$self->{defaultBuckets}}; $i++) {
+		my $defaultBucket = $self->{defaultBuckets}->[$i];
+		$out .= "<div class='hidden default bucket' data-bucket-id='$i' data-removable='$defaultBucket->{removable}'>";
+		$out .= "<div class='label'>$defaultBucket->{label}</div>";
+		$out .= "<ol class='answer'>";
+		for my $j ( @{$defaultBucket->{indices}} ) {
+			$out .= "<li data-shuffled-index='$j'>$self->{aggregateList}->[$j]</li>";
+		}
+		$out .= "</ol></div>";
+	}
+	
 	# buckets from past answers
-    for my $bucket ( @{$self->{bucketList}} ) {
-        $out .= "<div class='hidden past_answers bucket' data-bucket-id='$bucket->{bucket_id}' data-removable='$bucket->{removable}'>";
-        $out .= "<div class='label'>$bucket->{label}</div>"; 
-        $out .= "<ol class='answer'>";
-        
-        for my $index ( @{$bucket->{indices}} ) {
-            $out .= "<li data-shuffled-index='$index'>$self->{aggregateList}->[$index]</li>";
-        }
-        $out .= "</ol>";
-        $out .= "</div>"; 
-    }    
-    $out .= '</div>';
-    $out .= "<br clear='all'><div><a class='btn reset_buckets'>reset</a>";    
-    if ($self->{AllowNewBuckets} == 1) {
-        $out .= "<a class='btn add_bucket' data-ans='$self->{answerInputId}'>add bucket</a>";
-    }
+	for my $bucket ( @{$self->{bucketList}} ) {
+		$out .= "<div class='hidden past_answers bucket' data-bucket-id='$bucket->{bucket_id}' ";
+		$out .= "data-removable='$bucket->{removable}'>";
+		$out .= "<div class='label'>$bucket->{label}</div>";
+		$out .= "<ol class='answer'>";
+		
+		for my $index ( @{$bucket->{indices}} ) {
+			$out .= "<li data-shuffled-index='$index'>$self->{aggregateList}->[$index]</li>";
+		}
+		$out .= "</ol>";
+		$out .= "</div>";
+	}
+	$out .= '</div>';
+	$out .= "<br clear='all'><div><a class='btn reset_buckets'>reset</a>";
+	if ($self->{AllowNewBuckets} == 1) {
+		$out .= "<a class='btn add_bucket' data-ans='$self->{answerInputId}'>add bucket</a>";
+	}
 	$out .= "</div>";
-    
-    return $out;
+	
+	return $out;
 }
 
 sub TeX {
-    my $self = shift;
-    	
-    my $out = "";
-        
-    # default buckets;
-    for (my $i = 0; $i < @{ $self->{defaultBuckets} }; $i++) {
+	my $self = shift;
+	
+	my $out = "";
+	
+	# default buckets;
+	for (my $i = 0; $i < @{ $self->{defaultBuckets} }; $i++) {
 		$out .= "\n";
-        my $defaultBucket = $self->{defaultBuckets}->[$i];
+		my $defaultBucket = $self->{defaultBuckets}->[$i];
 		if ( @{$defaultBucket->{indices}} > 0 ) {
 			$out .= "\n\\hrule\n\\begin{itemize}";		
 			for my $j ( @{$defaultBucket->{indices}} ) {
@@ -166,7 +199,7 @@ sub TeX {
 			$out .= "\n\\end{itemize}";
 		}
 		$out .= "\n\\hrule\n";
-    }
-    return $out;
+	}
+	return $out;
 }
 1;
--- macros/draggableSubsets.pl
+++ macros/draggableSubsets.pl
@@ -1,15 +1,34 @@
-################################################################
+################################################################################
+# WeBWorK Online Homework Delivery System
+# Copyright &copy; 2000-2021 The WeBWorK Project, http://openwebwork.sf.net/
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of either: (a) the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version, or (b) the "Artistic License" which comes with this package.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
+# Artistic License for more details.
+################################################################################
+
 =head1 NAME
+
 draggableSubsets.pl
-  
+
 =head1 DESCRIPTION
-draggableSubsets.pl helps the instructor create a drag-and-drop environment in which a pre-specified set of elements may be dragged to different "buckets", effectively partitioning the original set into subsets.
+
+This macro helps the instructor create a drag-and-drop environment in which a pre-specified set
+of elements may be dragged to different "buckets", effectively partitioning the original set into subsets.
 
 =head1 TERMINOLOGY
+
 An HTML element into or out of which other elements may be dragged will be called a "bucket".
 An HTML element which houses a collection of buckets will be called a "bucket pool".
 
 =head1 USAGE
+
 To initialize a DraggableSubset bucket pool in a .pg problem, insert the line:
 
 $draggable = DraggableSubsets($full_set, $ans, Options1 => ..., Options2 => ...);
@@ -22,8 +41,13 @@ $draggable->Print
 
 within the BEGIN_TEXT / END_TEXT environment;
 
-$full_set, e.g. ["statement1", "statement2", ...], is an array reference to the list of elements, given as strings, in the original full set.
-$ans, e.g. [[1, 2, 3], [4, 5], ...], is an array reference to the list of array references corresponding to the correct answer which is a set of subsets. Each subset is specified via the indices of the elements according to their positions in $full_set, with the first element having index 0.
+$full_set, e.g. ["statement1", "statement2", ...], is an array reference to the list of elements, given as strings,
+in the original full set.
+
+$ans, e.g. [[1, 2, 3], [4, 5], ...], is an array reference to the list of array references
+corresponding to the correct answer which is a set of subsets.
+Each subset is specified via the indices of the elements according to their positions in $full_set,
+with the first element having index 0.
 
 Available Options:
 DefaultSubsets => <array reference>
@@ -33,14 +57,16 @@ AllowNewBuckets => 0 or 1
 Their usage is explained in the example below.
 
 =head1 EXAMPLE
+
 DOCUMENT();
 loadMacros(
-  "PGstandard.pl",
-  "MathObjects.pl",
-  "draggableSubsets.pl",
+"PGstandard.pl",
+"MathObjects.pl",
+"draggableSubsets.pl",
 );
 
 TEXT(beginproblem());
+
 $D3 = [
 "\(e\)", #0
 "\(r\)", #1
@@ -50,7 +76,7 @@ $D3 = [
 "\(sr^2\)", #5
 ];
 
-$subgroup = "e, s"; 
+$subgroup = "e, s";
 
 $subsets = [
 [0, 3],
@@ -60,39 +86,46 @@ $subsets = [
 
 $draggable = DraggableSubsets(
 $D3, # full set. Square brackets must be used.
-$subsets, # reference to array of arrays of indices, corresponding to correct set of subsets. Square brackets must be used.
+#
+$subsets, # reference to array of arrays of indices, corresponding to correct set of subsets.
+# Square brackets must be used.
+#
 DefaultSubsets => [ # default instructor-provided subsets. Default value = [].
 {
-    label => 'coset 1', # label of the bucket.
-    indices => [ 1, 3, 4, 5 ], # specifies pre-included elements in the bucket via their indices.
-    removable => 0 # specifies whether student may remove bucket.
+label => 'coset 1', # label of the bucket.
+indices => [ 1, 3, 4, 5 ], # specifies pre-included elements in the bucket via their indices.
+removable => 0 # specifies whether student may remove bucket.
 },
 {
-    label => 'coset 2',
-    indices => [ 0 ],
-    removable => 1
+label => 'coset 2',
+indices => [ 0 ],
+removable => 1
 },
 {
-    label => 'coset 3',
-    indices => [ 2 ],
-    removable => 1
+label => 'coset 3',
+indices => [ 2 ],
+removable => 1
 }
 ],
-# OrderedSubsets => 0, # OrderedSubsets => 0 means order of subsets does not matter. 1 means otherwise. (The order of elements within each subset never matters.) Default value = 0. 
-# AllowNewBuckets => 1, # AllowNewBuckets => 0 means no new buckets may be added by student. 1 means otherwise. Default value = 1. 
+# OrderedSubsets => 0, # means order of subsets does not matter. 1 means otherwise.
+# (The order of elements within each subset never matters.) Default value = 0.
+#
+# AllowNewBuckets => 0, # means no new buckets may be added by student. 1 means otherwise. Default value = 1.
 );
 
-Context()->texStrings;
+Context()->texStrings; # must be placed AFTER defining all DraggableSubsets.
 
 BEGIN_TEXT
 
 Let \[
 G=D_3=\lbrace e,r,r^2, s,sr,sr^2 \rbrace
 \]
-be the Dihedral group of order \(6\), where \(r\) is counter-clockwise rotation by \(2\pi/3\), and \(s\) is the reflection across the \(x\)-axis.
+be the Dihedral group of order \(6\), where \(r\) is counter-clockwise rotation by \(2\pi/3\),
+and \(s\) is the reflection across the \(x\)-axis.
 
 Partition \(G=D_3\) into $BBOLD right $EBOLD cosets of the subgroup
-\(H=\lbrace $subgroup \rbrace\).  Give your result by dragging the following elements into separate buckets, each corresponding to a coset.
+\(H=\lbrace $subgroup \rbrace\).  Give your result by dragging the following elements into separate buckets,
+each corresponding to a coset.
 
 $PAR
 \{ $draggable->Print \}
@@ -105,7 +138,9 @@ Context()->normalStrings;
 ANS($draggable->cmp);
 
 ENDDOCUMENT();
+
 =cut
+
 ################################################################
 
 loadMacros(
@@ -124,24 +159,24 @@ sub _draggableSubsets_init {
 package draggableSubsets;
 
 sub new {
-	my $self = shift; 
+	my $self = shift;
 	my $class = ref($self) || $self;
 	
 	# user arguments
-	my $set = shift; 
-	my $subsets = shift; 
+	my $set = shift;
+	my $subsets = shift;
 	my %options = (
 	DefaultSubsets => [],
 	OrderedSubsets => 0,
 	AllowNewBuckets => 1,
-    @_
-    );
+	@_
+	);
 	# end user arguments
 	
 	my $numProvided = scalar(@$set);
 	my @order = main::shuffle($numProvided);
 	my @unorder = main::invert(@order);
-
+	
 	my $shuffledSet = [ map {$set->[$_]} @order ];
 	
 	my $defaultBuckets = $options{DefaultSubsets};
@@ -149,26 +184,26 @@ sub new {
 	if (@$defaultBuckets) {
 		for my $defaultBucket (@$defaultBuckets) {
 			my $shuffledIndices = [ map {$unorder[$_]} @{ $defaultBucket->{indices} } ];
-			my $default_shuffled_bucket = { 
-				label => $defaultBucket->{label}, 
+			my $default_shuffled_bucket = {
+				label => $defaultBucket->{label},
 				indices => $shuffledIndices,
 				removable => $defaultBucket->{removable},
 			};
 			push(@$defaultShuffledBuckets, $default_shuffled_bucket);			
-		} 
+		}
 	} else {
-		push(@$defaultShuffledBuckets, [ { 
-			label => '', 
+		push(@$defaultShuffledBuckets, [ {
+			label => '',
 			indices => [ 0..$numProvided-1 ]
 		} ]);
 	}
-		
+	
 	my $answerInputId = main::NEW_ANS_NAME() unless $self->{answerInputId};	
 	my $ans_rule = main::NAMED_HIDDEN_ANS_RULE($answerInputId);
 	my $dnd = new DragNDrop(
-	$answerInputId, 
-	$shuffledSet, 
-	$defaultShuffledBuckets, 
+	$answerInputId,
+	$shuffledSet,
+	$defaultShuffledBuckets,
 	AllowNewBuckets => $options{AllowNewBuckets},
 	);	
 	
@@ -188,19 +223,19 @@ sub new {
 			$dnd->addBucket($indices->[0] != -1 ? $indices : [], label => $label, removable => $removable);
 		}
 	}	
-		
+	
 	my @shuffled_subsets_array = ();
 	for my $subset ( @$subsets ) {
 		my @shuffled_subset = map {$unorder[$_]} @$subset;
 		push(@shuffled_subsets_array, @$subset != 0 ? main::Set(join(',', @shuffled_subset)) : main::Set());
 	}	
 	my $shuffled_subsets = main::List(@shuffled_subsets_array);
-		
+	
 	$self = bless {
 		set => $set,
 		shuffledSet => $shuffledSet,
 		numProvided => $numProvided,
-		order => \@order, 
+		order => \@order,
 		unordered => \@unorder,
 		shuffled_subsets => $shuffled_subsets,
 		answerInputId => $answerInputId,
@@ -221,21 +256,25 @@ sub Print {
 	if ($main::displayMode ne "TeX") {
 		# HTML mode
 		return join("\n",
-			'<div style="min-width:750px;">',
-			$ans_rule,
-			$self->{dnd}->HTML,
-			'<br clear="all" />',
-			'</div>',
+		'<div style="min-width:750px;">',
+		$ans_rule,
+		$self->{dnd}->HTML,
+		'<br clear="all" />',
+		'</div>',
 		);
 	} else {
 		# TeX mode
-	    return $self->{dnd}->TeX;		
+		return $self->{dnd}->TeX;		
 	}
 }
 
 sub cmp {
 	my $self = shift;	
-	return $self->{shuffled_subsets}->cmp(ordered => $self->{OrderedSubsets}, removeParens => 1, partialCredit => 1)->withPreFilter(sub {$self->prefilter(@_)})->withPostFilter(sub {$self->filter(@_)});
+	
+	return $self->{shuffled_subsets}
+	->cmp(ordered => $self->{OrderedSubsets}, removeParens => 1, partialCredit => 1)
+	->withPreFilter(sub {$self->prefilter(@_)})
+	->withPostFilter(sub {$self->filter(@_)});
 }
 
 sub prefilter {
@@ -253,7 +292,7 @@ sub prefilter {
 	}
 	
 	$anshash->{student_ans} = main::List(@studentAnsArray);
-		
+	
 	return $anshash;
 }
 
@@ -264,16 +303,16 @@ sub filter {
 	my @student = ( $anshash->{original_student_ans} =~ /(\([^\(\)]*\)|-?\d+)/g );
 	my @correct = ( $anshash->{correct_ans} =~ /({[^{}]*}|-?\d+)/g );
 	
-	$anshash->{correct_ans_latex_string} = join (",", map { 
-		"\\{\\text{".join(",", (map { 
+	$anshash->{correct_ans_latex_string} = join (",", map {
+		"\\{\\text{".join(",", (map {
 			$_ != -1 ? $self->{shuffledSet}->[$_] : ''
-		} (split(',', $_ =~ s/{|}//gr)) ))."}\\}" 
+		} (split(',', $_ =~ s/{|}//gr)) ))."}\\}"
 	} @correct);
 	
-	$anshash->{preview_latex_string} = join (",", map { 
-		"\\{\\text{".join(",", (map { 
+	$anshash->{preview_latex_string} = join (",", map {
+		"\\{\\text{".join(",", (map {
 			$_ != -1 ? $self->{shuffledSet}->[$_] : ''
-		} (split(',', $_ =~ s/\(|\)//gr)) ))."}\\}" 
+		} (split(',', $_ =~ s/\(|\)//gr)) ))."}\\}"
 	} @student);
 	
 	$anshash->{student_ans} = "(see preview)";
