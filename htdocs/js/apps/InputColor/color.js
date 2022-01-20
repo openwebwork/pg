b7c67753061ac7b372eb34bcfc9141fa43ec7664
Glenn Rice <grice1@missouriwestern.edu>
2022-01-19 20:23:57 -0600

    Change the way that coloring of inputs is done for correct and incorrect answers.  Instead of creating a global javascript variable, use data attributes.
    Also, instead of using inline javascript to make clicking on the
    correct/incorrect link set focus on the input, add an event listener.
    This is done from htdocs/js/apps/InputColor/color.js, so perhaps that
    file is not properly named anymore.
    
    Note that if MathQuill is enabled this is handled for mathquill inputs
    in htdocs/js/apps/MathQuill/mqeditor.js.  It did so before, but in a
    hackish way.  Now it is much cleaner.
    
    As with all of these changes this is also designed to work with webwork3
    and the standalone renderer.

--- htdocs/js/apps/InputColor/color.js
+++ htdocs/js/apps/InputColor/color.js
@@ -1,36 +1,42 @@
-/* 
- * color.js
- *
- * for coloring the input elements with the proper color based on whether they are correct or incorrect
- *
- * Originally by ghe3
- * Edited by dpvc 2014-08
- */
+// For coloring the input elements with the proper color based on whether they are correct or incorrect.
 
-function color_inputs(correct,incorrect) {
-    var className = {};
-    var i, m, inputs, input, name;
-    var addClass = function (input,name) {
-	if (input) {
-	    if (input.className == "") {input.className = name} else {input.className += " "+name}
-	}
-    };
+(() => {
+	const setupAnswerLink = (answerLink) => {
+		const answerId = answerLink.dataset.answerId;
+		const answerInput = document.getElementById(answerId);
 
-    for (i = 0, m = correct.length; i < m; i++) {
-	addClass(document.getElementById(correct[i]),"correct");
-	className[correct[i]] = "correct";
-    }
-    for (i = 0, m = incorrect.length; i < m; i++) {
-	addClass(document.getElementById(incorrect[i]),"incorrect");
-	className[incorrect[i]] = "incorrect";
-    }
-    
-    inputs = document.getElementsByTagName("input");
-    for (i = 0, m = inputs.length; i < m; i++) {
-	input = inputs[i];
-	if (!input.hidden && input.name === input.id) {
-	    name = input.name.replace(/^(MaTrIx_MuLtIaNsWeR|MaTrIx|MuLtIaNsWeR)_/,"").replace(/(_\d+)+$/,"");
-	    if (name !== input.name && className[name]) addClass(input,className[name]);
-	}
-    }
-}
+		const type = answerLink.parentNode.classList.contains('ResultsWithoutError') ? 'correct' : 'incorrect';
+		document.querySelectorAll(`input[name*=${answerId}],select[name*=${answerId}`)
+			.forEach((input) => input.classList.add(type));
+
+		if (answerInput) {
+			answerLink.addEventListener('click', (e) => {
+				e.preventDefault();
+				answerInput.focus();
+			});
+		} else {
+			answerLink.href = '';
+		}
+	};
+
+	// Color inputs already on the page.
+	document.querySelectorAll('td a[data-answer-id]').forEach(setupAnswerLink);
+
+	// Deal with inputs that are added to the page later.
+	const observer = new MutationObserver((mutationsList) => {
+		mutationsList.forEach((mutation) => {
+			mutation.addedNodes.forEach((node) => {
+				if (node instanceof Element) {
+					if (node.type && node.type.toLowerCase() === 'td' && node.firstElementChild
+						&& node.firstElementChild.type.toLowerCase() == 'a' && node.firstElementChild.dataset.answerId)
+						setupAnswerLink(node.firstElementChild);
+					else node.querySelectorAll('td a[data-answer-id]').forEach(setupAnswerLink);
+				}
+			});
+		});
+	});
+	observer.observe(document.body, { childList: true, subtree: true });
+
+	// Stop the mutation observer when the window is closed.
+	window.addEventListener('unload', () => observer.disconnect());
+})();
