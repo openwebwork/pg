84b66adda38622f463709dfd0cefbe670588578d
Nathan Wallach <tani@technion.ac.il>
2022-05-26 16:50:59 +0300

    Limit loop to compute factorial to n<=170, for n>170 return Perl Inf, as that would be the result of the calculation used for any such n due to overflow of the Perl floating point data type.

--- lib/Parser/UOP/factorial.pm
+++ lib/Parser/UOP/factorial.pm
@@ -25,6 +25,7 @@ sub _eval {
   my $n = shift; my $f = 1;
   $self->Error("Factorial can only be taken of (non-negative) integers")
     unless $n =~ m/^\d+$/;
+  return Inf if $n > 170;
   while ($n > 0) {$f *= $n; $n--}
   return $f;
 }
