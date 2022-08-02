a9d358fd1a1e85806cbef703dff16c8718ac78ea
Peter Staab <peter.staab@gmail.com>
2022-08-02 09:41:13 -0400

    initial template for SigFig context

---
+++ macros/contextSigFig.pl
@@ -0,0 +1,37 @@
+################################################################################
+# WeBWorK Online Homework Delivery System
+# Copyright &copy; 2000-2022 The WeBWorK Project, https://github.com/openwebwork
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of either: (a) the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version, or (b) the "Artistic License" which comes with this package.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
+# Artistic License for more details.
+################################################################################
+
+=head1 NAME
+
+contextSigFig.pl - context for answers with significant figures.
+
+=head1 DESCRIPTION
+
+
+
+=head1 USAGE
+
+
+=head1 AUTHORS
+
+Steven Fiedler (Fitchburg State Unviersity)
+
+=cut
+
+loadMacros('MathObjects.pl');
+
+
+
+1;
