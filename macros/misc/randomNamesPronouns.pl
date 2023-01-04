564917a11234ff078f1b7dbee84ccd1fd6fce14b
Peter Staab <peter.staab@gmail.com>
2023-01-04 11:20:52 -0500

    Cleanup of documentation
    cleanup of docmentation
    
    
    Cleanup of documentation
    
    
    Cleanup of documentation

--- macros/misc/randomNamesPronouns.pl
+++ macros/misc/randomNamesPronouns.pl
@@ -1,3 +1,4 @@
+
 =head1 NAME
 
 randomNamesPronouns.pl - Load macros for random names.
@@ -11,202 +12,210 @@ randomNamesPronouns.pl - Load macros for random names.
 randomNamesPronouns.pl provides a randomName function that generates a random name with pronouns.  In addition,
 there is the capability of providing pronouns with and without capitilization and verb conjugation.
 
-Note: this idea was taken from the PCCmacros.pl RandomName subroutine to extend to
-the handling of pronouns.
+Note: this idea and the names wer taken from the PCCmacros.pl RandomName subroutine to extend to
+the handling of pronouns. Most of the names in here were taken from that macro.  Many thanks to those
+who worked on that macro.
 
 =head2 USAGE
 
 First load the C<randomNamesPronouns> with
 
-C<loadMacros('randomNamesPronouns.pl');>
+	loadMacros('randomNamesPronouns.pl');
 
-and then call the randomName subroutine
+and then call the randomPerson subroutine
 
-C< $p1 = randomName() >
+	$p1 = randomPerson()
 
 The variable C<$p1> is now a C<Person> object with methods to access the names, pronouns
 and verb conjugation.  It is can be used within a problem as
 
-C<BEGIN_PGML>
-C<[@ $p1->name @] [@ $p1->verb('travels','travel') @] 1.5 miles to school.  After school,
-[$p1->pronoun] then [$p1->]
-
+	BEGIN_PGML
+	[@ $p1->name @] [@ $p1->verb('travel') @] 1.5 miles to school.  After school,
+	[$p1->pronoun] then [@$p1->verb('goes','go')@] to work.
+	END_PGML
 
 =cut
 
 my $names = {
-	'Aaliyah'=>'she',
-	'Aaron'=>'he',
-	'Adrian'=>'she',
-	'Aiden' => 'they',
-	'Alejandro'=>'he',
-	'Aleric'=>'he',
-	'Alex'=>'they',
-	'Alisa'=>'she',
-	'Alyson'=>'she',
-	'Amber'=>'she',
-	'Andrew'=>'he',
-	'Annaly'=>'she',
-	'Anthony'=>'he',
-	'Ashley'=>'she',
-	'Barbara'=>'she',
-	'Benjamin'=>'he',
-	'Blake'=>'he',
-	'Bobbi'=>'she',
-	'Brad'=>'he',
-	'Brent'=>'he',
-	'Briana'=>'she',
-	'Candi'=>'she',
-	'Carl'=>'he',
-	'Carly'=>'she',
-	'Carmen'=>'she',
-	'Casandra'=>'she',
-	'Charity'=>'she',
-	'Charlotte'=>'she',
-	'Cheryl'=>'she',
-	'Chris'=>'he',
-	'Cody'=>'he',
-	'Connor'=>'he',
-	'Corey'=>'he',
-	'Daniel'=>'he',
-	'Dave'=>'he',
-	'Dawn'=>'she',
-	'Dennis'=>'he',
-	'Derick'=>'he',
-	'Devon'=>'he',
-	'Diane'=>'she',
-	'Don' => 'they',
-	'Donna'=>'she',
-	'Douglas'=>'he',
-	'Dylan'=>'they',
-	'Eileen'=>'she',
-	'Eliot'=>'they',
-	'Elishua'=>'she',
-	'Emiliano'=>'he',
-	'Emily'=>'she',
-	'Eric'=>'he',
-	'Evan'=>'he',
-	'Fabrienne'=>'she',
-	'Farshad'=>'he',
-	'Gosheven'=>'he',
-	'Grant'=>'he',
-	'Gregory'=>'he',
-	'Gustav'=>'he',
-	'Haley'=>'she',
-	'Hannah'=>'she',
-	'Hayden'=>'he',
-	'Heather'=>'she',
-	'Henry'=>'he',
-	'Holli'=>'she',
-	'Huynh'=>'he',
-	'Irene'=>'she',
-	'Ivan'=>'he',
-	'Izabelle'=>'she',
-	'James'=>'he',
-	'Janieve'=>'she',
-	'Jay'=>'he',
-	'Jeff'=>'he',
-	'Jenny'=>'she',
-	'Jerry'=>'he',
-	'Jessica'=>'she',
-	'Jon'=>'he',
-	'Jordan'=>'they',
-	'Joseph'=>'he',
-	'Joshua'=>'he',
-	'Julie'=>'she',
-	'Kandace'=>'she',
-	'Kara'=>'she',
-	'Katherine'=>'she',
-	'Kayla'=>'she',
-	'Ken'=>'he',
-	'Kenji'=>'he',
-	'Kim'=>'she',
-	'Kimball'=>'he',
-	'Kristen'=>'she',
-	'Kurt'=>'he',
-	'Kylie'=>'she',
-	'Kyrie'=>'he',
-	'Laney'=>'she',
-	'Laurie'=>'she',
-	'Lesley'=>'she',
-	'Lily'=>'she',
-	'Lin'=>'he',
-	'Lindsay'=>'she',
-	'Lisa'=>'she',
-	'Luc'=>'they',
-	'Malik'=>'he',
-	'Marc'=>'he',
-	'Maria'=>'she',
-	'Martha'=>'she',
-	'Matthew'=>'he',
-	'Matty'=>'they',
-	'Max'=>'they',
-	'Maygen'=>'she',
-	'Michael'=>'he',
-	'Michele'=>'she',
-	'Morah'=>'she',
-	'Nathan'=>'he',
-	'Neil'=>'he',
-	'Nenia'=>'she',
-	'Nicholas'=>'he',
-	'Nina'=>'she',
-	'Olivia'=>'she',
-	'Page'=>'she',
-	'Parnell'=>'he',
-	'Penelope'=>'she',
-	'Perlia'=>'she',
-	'Peter'=>'he',
-	'Phil'=>'he',
-	'Priscilla'=>'she',
-	'Randi'=>'he',
-	'Ravi'=>'he',
-	'Ray'=>'they',
-	'Rebecca'=>'she',
-	'Renee'=>'she',
-	'Rita'=>'she',
-	'Ronda'=>'she',
-	'Ross'=>'he',
-	'Ryan'=>'he',
-	'Samantha'=>'she',
-	'Sarah'=>'she',
-	'Scot'=>'he',
-	'Sean'=>'he',
-	'Sebastian'=>'he',
-	'Selena'=>'she',
-	'Shane'=>'he',
-	'Sharell'=>'she',
-	'Sharnell'=>'she',
-	'Sherial'=>'she',
-	'Stephanie'=>'she',
-	'Stephen'=>'he',
-	'Subin'=>'she',
-	'Sydney'=>'she',
-	'Tammy'=>'she',
-	'Teresa'=>'she',
-	'Thanh'=>'he',
-	'Tien'=>'he',
-	'Tiffany'=>'she',
-	'Timothy'=>'he',
-	'Tracey'=>'she',
-	'Virginia'=>'she',
-	'Wendy'=>'she',
-	'Wenwu'=>'he',
-	'Will'=>'he'
+	'Aaliyah'   => 'she',
+	'Aaron'     => 'he',
+	'Adrian'    => 'she',
+	'Aiden'     => 'they',
+	'Alejandro' => 'he',
+	'Aleric'    => 'he',
+	'Alex'      => 'they',
+	'Alisa'     => 'she',
+	'Alyson'    => 'she',
+	'Amber'     => 'she',
+	'Andrew'    => 'he',
+	'Annaly'    => 'she',
+	'Anthony'   => 'he',
+	'Ashley'    => 'she',
+	'Barbara'   => 'she',
+	'Benjamin'  => 'he',
+	'Blake'     => 'he',
+	'Bobbi'     => 'she',
+	'Brad'      => 'he',
+	'Brent'     => 'he',
+	'Briana'    => 'she',
+	'Candi'     => 'she',
+	'Carl'      => 'he',
+	'Carly'     => 'she',
+	'Carmen'    => 'she',
+	'Casandra'  => 'she',
+	'Charity'   => 'she',
+	'Charlotte' => 'she',
+	'Cheryl'    => 'she',
+	'Chris'     => 'he',
+	'Cody'      => 'he',
+	'Connor'    => 'he',
+	'Corey'     => 'he',
+	'Daniel'    => 'he',
+	'Dave'      => 'he',
+	'Dawn'      => 'she',
+	'Dennis'    => 'he',
+	'Derick'    => 'he',
+	'Devon'     => 'he',
+	'Diane'     => 'she',
+	'Don'       => 'they',
+	'Donna'     => 'she',
+	'Douglas'   => 'he',
+	'Dylan'     => 'they',
+	'Eileen'    => 'she',
+	'Eliot'     => 'they',
+	'Elishua'   => 'she',
+	'Emiliano'  => 'he',
+	'Emily'     => 'she',
+	'Eric'      => 'he',
+	'Evan'      => 'he',
+	'Fabrienne' => 'she',
+	'Farshad'   => 'he',
+	'Gosheven'  => 'he',
+	'Grant'     => 'he',
+	'Gregory'   => 'he',
+	'Gustav'    => 'he',
+	'Haley'     => 'she',
+	'Hannah'    => 'she',
+	'Hayden'    => 'he',
+	'Heather'   => 'she',
+	'Henry'     => 'he',
+	'Holli'     => 'she',
+	'Huynh'     => 'he',
+	'Irene'     => 'she',
+	'Ivan'      => 'he',
+	'Izabelle'  => 'she',
+	'James'     => 'he',
+	'Janieve'   => 'she',
+	'Jay'       => 'he',
+	'Jeff'      => 'he',
+	'Jenny'     => 'she',
+	'Jerry'     => 'he',
+	'Jessica'   => 'she',
+	'Jon'       => 'he',
+	'Jordan'    => 'they',
+	'Joseph'    => 'he',
+	'Joshua'    => 'he',
+	'Julie'     => 'she',
+	'Kandace'   => 'she',
+	'Kara'      => 'she',
+	'Katherine' => 'she',
+	'Kayla'     => 'she',
+	'Ken'       => 'he',
+	'Kenji'     => 'he',
+	'Kim'       => 'she',
+	'Kimball'   => 'he',
+	'Kristen'   => 'she',
+	'Kurt'      => 'he',
+	'Kylie'     => 'she',
+	'Kyrie'     => 'he',
+	'Laney'     => 'she',
+	'Laurie'    => 'she',
+	'Lesley'    => 'she',
+	'Lily'      => 'she',
+	'Lin'       => 'he',
+	'Lindsay'   => 'she',
+	'Lisa'      => 'she',
+	'Luc'       => 'they',
+	'Malik'     => 'he',
+	'Marc'      => 'he',
+	'Maria'     => 'she',
+	'Martha'    => 'she',
+	'Matthew'   => 'he',
+	'Matty'     => 'they',
+	'Max'       => 'they',
+	'Maygen'    => 'she',
+	'Michael'   => 'he',
+	'Michele'   => 'she',
+	'Morah'     => 'she',
+	'Nathan'    => 'he',
+	'Neil'      => 'he',
+	'Nenia'     => 'she',
+	'Nicholas'  => 'he',
+	'Nina'      => 'she',
+	'Olivia'    => 'she',
+	'Page'      => 'she',
+	'Parnell'   => 'he',
+	'Penelope'  => 'she',
+	'Perlia'    => 'she',
+	'Peter'     => 'he',
+	'Phil'      => 'he',
+	'Priscilla' => 'she',
+	'Randi'     => 'he',
+	'Ravi'      => 'he',
+	'Ray'       => 'they',
+	'Rebecca'   => 'she',
+	'Renee'     => 'she',
+	'Rita'      => 'she',
+	'Ronda'     => 'she',
+	'Ross'      => 'he',
+	'Ryan'      => 'he',
+	'Samantha'  => 'she',
+	'Sarah'     => 'she',
+	'Scot'      => 'he',
+	'Sean'      => 'he',
+	'Sebastian' => 'he',
+	'Selena'    => 'she',
+	'Shane'     => 'he',
+	'Sharell'   => 'she',
+	'Sharnell'  => 'she',
+	'Sherial'   => 'she',
+	'Stephanie' => 'she',
+	'Stephen'   => 'he',
+	'Subin'     => 'she',
+	'Sydney'    => 'she',
+	'Tammy'     => 'she',
+	'Teresa'    => 'she',
+	'Thanh'     => 'he',
+	'Tien'      => 'he',
+	'Tiffany'   => 'she',
+	'Timothy'   => 'he',
+	'Tracey'    => 'she',
+	'Virginia'  => 'she',
+	'Wendy'     => 'she',
+	'Wenwu'     => 'he',
+	'Will'      => 'he'
 };
 
+=head2 randomPerson
+
+Returns a person as a Person object from a list in the macro.
+
+=cut
+
 sub randomPerson {
-	my @names = lex_sort(keys(%$names));
-	my $random_name = list_random(@names);
-	return Person->new({ name => $random_name, pronoun => $names->{$random_name}});
+	my $random_name = list_random(keys(%$names));
+	return Person->new({ name => $random_name, pronoun => $names->{$random_name} });
 }
 
-=pod
-=head2 package Person
+=head2 CONSTRUCTOR Person
 
-This makes a Person class to handle name and pronouns of a Person.
+This makes a Person object to handle name and pronouns of a Person.
 
-Make a person with C<new Person({ name => 'Roger', pronoun => 'he'})> as an example. This is
-often used with the C<randomPerson> method which returns a blessed Person object
+Make a person with
+
+	Person->new({ name => 'Roger', pronoun => 'he'})
+
+as an example. This is often used with the C<randomPerson> method which returns a blessed Person object
 which can be used in problems to write a problem with a random name with pronouns
 and verb conjugation.
 
@@ -216,80 +225,97 @@ package Person;
 
 sub new {
 	my ($class, $p) = @_;
+	my @v = grep { $p->{pronoun} eq $_ } qw/he she they/;
+	return Value::Error("The pronoun must be either he, she or they. You passed in $p->{pronoun}")
+		if scalar(@v) != 1;
 	my $self = {
-		_name => $p->{name},
+		_name    => $p->{name},
 		_pronoun => $p->{pronoun}
 	};
 	bless $self, $class;
 	return $self;
 }
 
-=pod
-=head3 name
+=head2 name
 
 This returns the name of the person.
 
-C< my $p = new Person({ name => 'Roger', pronoun => 'he'})>
+	my $p = new Person({ name => 'Roger', pronoun => 'he'});
 
-C< $p->name> returns the name.
+	$p->name;
 
+returns the name ('Roger').
 =cut
 
 sub name { return shift->{_name}; }
 
-=pod
-=head3 pronoun
+=head2 pronoun
 
 This returns the pronoun as a lower case.
 
-C<$p->pronoun> returns the pronoun. In this case 'he'.
+	$p->pronoun;
+
+returns the pronoun. In this case 'he'.
 
 =cut
 
 sub pronoun { return shift->{_pronoun}; }
 
-=pod
-=head3 Pronoun
+=head2 Pronoun
 
 This returns the pronoun as an upper case.
 
-C<$p->Pronoun> returns the upper case pronoun. In this case 'He'.
+	$p->Pronoun;
+
+returns the upper case pronoun. In this case 'He'.
 
 =cut
 
 sub Pronoun { return ucfirst(shift->{_pronoun}); }
 
-=pod
-=head3 verb
+=head2 verb
 
-Outputs the correct conjugation of the verb.  If only one verb is passed in, it should
+Returns the correct conjugation of the verb.  If only one verb is passed in, it should
 be regular and the plural (without an s) version.
 
-For example if C<$p1 = new Person({ name => 'Roger', pronoun => 'he' })> and
-C<$p2 = new Person({ name => 'Max', pronoun 'they'})> then
+For example
 
-C<$p1->verb('find')> outputs C<'finds'>
+	$p1 = new Person({ name => 'Roger', pronoun => 'he' });
+	$p2 = new Person({ name => 'Max', pronoun => 'they'});
 
-C<$p2->vert('find')> outputs C<'find'>
+	$p1->verb('find');
+
+returns 'finds'
+
+	$p2->verb('find')
+
+returns 'find'
 
 
 If two arguments are passed in, they should be the singular and plural forms of the
 verbs in that order.
 
-For example if C<$p1 = new Person({ name => 'Roger', pronoun => 'he' })> and
-C<$p2 = new Person({ name => 'Max', pronoun 'they'})> then
+For example if
 
-C<$p1->verb('are', 'is')> outputs C<'is'>
+	$p1 = new Person({ name => 'Roger', pronoun => 'he' });
+	$p2 = new Person({ name => 'Max', pronoun 'they'});
 
-C<$p2->verb('are', 'is')> outputs C<'are'>
+	$p1->verb('is', 'are');
+
+returns 'is'
+
+	$p2->verb('is', 'are');
+
+returns C<'are'>
 
 =cut
 
 sub verb {
 	my ($self, $sing, $plur) = @_;
-	return defined($plur) ?
-		($self->{_pronoun} eq 'they' ? $plur : $sing) :
-		($self->{_pronoun} eq 'they' ? $sing : $sing . 's');
+	return
+		defined($plur)
+		? ($self->{_pronoun} eq 'they' ? $plur : $sing)
+		: ($self->{_pronoun} eq 'they' ? $sing : $sing . 's');
 }
 
 1;
--- t/macros_misc/random_names_pronouns.t
+++ t/macros_misc/random_names_pronouns.t
@@ -7,30 +7,35 @@ do "$ENV{PG_ROOT}/t/build_PG_envir.pl";
 
 loadMacros('randomNamesPronouns.pl');
 
-my $p1 = Person->new({ name => 'Fred', pronoun => 'he' });
+my $p1 = Person->new({ name => 'Fred',      pronoun => 'he' });
 my $p2 = Person->new({ name => 'Gabriella', pronoun => 'she' });
-my $p3 = Person->new({ name => 'Kai', pronoun => 'they' });
+my $p3 = Person->new({ name => 'Kai',       pronoun => 'they' });
 
-is($p1->name, 'Fred', 'Test the name method for Fred');
-is($p1->pronoun, 'he', 'Test the pronoun method for Fred');
-is($p1->Pronoun, 'He', 'Test the pronoun method for Fred');
-is($p1->verb('find'), 'finds', 'Tests the conjugation of the verb find for Fred.');
-is($p1->verb('is','are'), 'is', 'Tests the conjugation of the verb is for Fred.');
+is($p1->name,              'Fred',  'Test the name method for Fred');
+is($p1->pronoun,           'he',    'Test the pronoun method for Fred');
+is($p1->Pronoun,           'He',    'Test the pronoun method for Fred');
+is($p1->verb('find'),      'finds', 'Tests the conjugation of the verb find for Fred.');
+is($p1->verb('is', 'are'), 'is',    'Tests the conjugation of the verb is for Fred.');
 
+is($p2->name,              'Gabriella', 'Test the name method for Gabriella');
+is($p2->pronoun,           'she',       'Test the pronoun method for Gabriella');
+is($p2->Pronoun,           'She',       'Test the pronoun method for Gabriella');
+is($p2->verb('find'),      'finds',     'Tests the conjugation of the verb find for Gabriella.');
+is($p2->verb('is', 'are'), 'is',        'Tests the conjugation of the verb is for Gabriella.');
 
-is($p2->name, 'Gabriella', 'Test the name method for Gabriella');
-is($p2->pronoun, 'she', 'Test the pronoun method for Gabriella');
-is($p2->Pronoun, 'She', 'Test the pronoun method for Gabriella');
-is($p2->verb('find'), 'finds', 'Tests the conjugation of the verb find for Gabriella.');
-is($p2->verb('is','are'), 'is', 'Tests the conjugation of the verb is for Gabriella.');
-
-is($p3->name, 'Kai', 'Test the name method for Kai');
-is($p3->pronoun, 'they', 'Test the pronoun method for Kai');
-is($p3->Pronoun, 'They', 'Test the pronoun method for Kai');
-is($p3->verb('find'), 'find', 'Tests the conjugation of the verb find for Kai.');
-is($p3->verb('is','are'), 'are', 'Tests the conjugation of the verb is for Kai.');
+is($p3->name,              'Kai',  'Test the name method for Kai');
+is($p3->pronoun,           'they', 'Test the pronoun method for Kai');
+is($p3->Pronoun,           'They', 'Test the pronoun method for Kai');
+is($p3->verb('find'),      'find', 'Tests the conjugation of the verb find for Kai.');
+is($p3->verb('is', 'are'), 'are',  'Tests the conjugation of the verb is for Kai.');
 
 my $rando = randomPerson();
 is(ref $rando, 'Person', 'Check that the randomPerson method returns an object of Person class');
+
+like(
+		dies { Person->new({ name => 'Head', pronoun => 'xxx'}) },
+		qr/The pronoun must be/,
+		"An invalid pronoun is passed in."
+	);
 
 done_testing;
