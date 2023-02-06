d405ede3803938919608e2a8c1fd5dc9bfd1d0f5
Glenn Rice <grice1@missouriwestern.edu>
2023-02-06 08:24:00 -0600

    Switch the drag and drop macros from using jquery.nestable to using sortablejs
    The sortablejs library works much better, and is a library that is
    maintained.  jquery.nestable has never been maintained, and was never
    even intended to be used in production.
    
    In addition all dependence on jQuery has been removed.  The sortablejs
    library is the only third party dependency.
    
    The draggableProof.pl and draggableSubsets.pl macros have been
    completely rewritten to fix issues seen with them.  Currently hardcopy
    is broken for answers (both student and correct).  The current macros
    do not support custom checkers, and due to limitations of the current
    design can not be extended to do so without this rewrite.
    
    There were several options added to the macros.  These options allow for
    customization of various textual components of the javascript output.
    These options would be useful for writing a problem for another
    language utilizing the macro.
    
    A "cmpOptions" option was added to both macros.  This can be used to
    conveniently pass options that will then be directly passed on to the
    cmp method.
    
    The previous `Print` method of the macros is now the `ans_rule` method.
    The `Print` method still exists, but just calls the `ans_rule` method.
    The addition of the `ans_rule` method makes it so that you can use the
    macro in PGML via `[_]{$draggable}` where `$draggable` is a
    DraggableProof or DraggableSubset object.  Unfortunately, if you want
    problems to be compatible with older versions of PG you will still need
    to use the `Print` method (things done wrong!).
    
    Note that the dependence of the macros on the PGchoicemacros.pl macro
    has also been removed.  However, the draggableProof.pl macro still loads
    it.  This is because some of the OPL problems that use the macro use the
    methods from the PGchoicemacros.pl macro without loading it directly.

--- htdocs/js/apps/DragNDrop/dragndrop.js
+++ htdocs/js/apps/DragNDrop/dragndrop.js
@@ -1,146 +1,187 @@
-(function() {
-	class DragNDropBucket {
-		constructor(pgData) {
-			this.answerInputId = pgData['answerInputId'];
-			this.bucketId = pgData['bucketId'];
-			this.label = pgData['label'] || '';
-			this.removable = pgData['removable'];
-			this.bucketPool = $('.dd-bucket-pool[data-ans="' + this.answerInputId + '"]').first()[0];
+'use strict';
 
-			const $bucketPool = $(this.bucketPool);
-			const $newBucket = this._newBucket(
-				this.bucketId,
-				this.label,
-				this.removable,
-				$bucketPool.find('.dd-hidden.dd-past-answers.dd-bucket[data-bucket-id="' + this.bucketId + '"]')
-			);
+(() => {
+	class BucketPool {
+		constructor(el) {
+			// Set a marker in the element dataset to prevent another BucketPool object from being created for it.
+			el.dataset.bucketPoolInitialized = 'true';
 
-			$bucketPool.append($newBucket);
+			this.answerName = el.dataset.answerName ?? '';
+			this.buckets = [];
+			this.removeButtonText = el.dataset.removeButtonText ?? 'Remove';
 
-			const el = this;
-
-			$newBucket.find('.dd').nestable({
-				group: el.answerInputId,
-				maxDepth: 1,
-				scroll: true,
-				callback: function() {el._nestableUpdate();}
-			});
-			this._nestableUpdate();
-			this._ddUpdate();
-		}
-
-		_newBucket(bucketId, label, removable, $bucketHtmlElement) {
-			const $newBucket = $('<div id="nestable-' + bucketId + '-container" class="dd-container"></div>');
-
-			$newBucket.attr('data-bucket-id', bucketId);
-			$newBucket.append($('<div class="nestable-label">' + label + '</div>'));
-			$newBucket.append($('<div class="dd" data-bucket-id="' + bucketId + '"></div>'));
-
-			if (removable != 0) {
-				$newBucket.append($('<button type="button" class="btn btn-secondary dd-remove-bucket">Remove</button>'));
+			this.answerInput = el.parentElement.querySelector(`input[name="${this.answerName}"]`);
+			if (!this.answerInput) {
+				// This should not happen if using the macros.
+				alert(`FATAL ERROR: Unable to find answer input corresponding to ${this.answerName}.`);
+				return;
 			}
 
-			if ($bucketHtmlElement.find('ol.dd-answer li').length) {
-				const $ddList = $('<ol class="dd-list"></ol>');
+			this.bucketContainer = document.createElement('div');
+			this.bucketContainer.classList.add('dd-pool-bucket-container');
+			el.prepend(this.bucketContainer);
 
-				$bucketHtmlElement.find('ol.dd-answer li').each(function() {
-					const $item = $('<li><div class="dd-handle">' + $(this).html() + '</div></li>');
+			this.itemList = JSON.parse(el.dataset.itemList ?? '[]');
+			this.defaultState = JSON.parse(el.dataset.defaultState ?? '[]');
+			this.labelFormat = el.dataset.labelFormat;
 
-					$item.addClass('dd-item').attr('data-shuffled-index', $(this).attr('data-shuffled-index'));
-					$ddList.append($item);
-				});
-				$newBucket.find('.dd').first().append($ddList);
-			}
-			$newBucket.css('background-color', 'hsla(' + ((100 + (bucketId)*100) % 360) + ', 40%, 90%, 1)');
-			return $newBucket;
-		}
-
-		_nestableUpdate() {
-			const buckets = [];
-
-			$(this.bucketPool).find('.dd').each(function() {
-				const list = [];
-
-				$(this).find('li.dd-item').each(function() {
-					list.push($(this).attr('data-shuffled-index'));
-				});
-				if (list.length) {
-					buckets.push('(' + list.join(",") + ')');
-				} else {
-					buckets.push('(-1)');
+			if (this.answerInput.value) {
+				// Need to check for things like (3,2,1) for backwards compatibility.  Now it will be {3,2,1}.
+				const matches = this.answerInput.value.match(/((?:\{|\()[^\{\}\(\)]*(?:\}|\)))/g);
+				for (const match of matches) {
+					const i = this.buckets.length;
+					const bucket = {
+						removable: i < this.defaultState.length ? this.defaultState[i].removable : 1,
+						label: i < this.defaultState.length ? this.defaultState[i].label : '',
+						indices: match
+							.replaceAll(/\{|\}|\(|\)/g, '')
+							.split(',')
+							.filter((index) => index !== '')
+					};
+					this.buckets.push(new Bucket(this, i, bucket));
 				}
+			} else {
+				for (const bucket of this.defaultState) {
+					this.buckets.push(new Bucket(this, this.buckets.length, bucket));
+				}
+			}
+			this.updateAnswerInput();
+
+			el.querySelector('.dd-add-bucket')?.addEventListener('click', () => {
+				// When buckets are removed and added the id's may not be sequential anymore.  So the bucket count
+				// cannot directly be used, and an id needs to be found that is not already in use.
+				let id = 0;
+				for (const bucketId of this.buckets.map((b) => b.id).sort()) {
+					if (id != bucketId) break;
+					++id;
+				}
+
+				this.buckets.push(new Bucket(this, id, { removable: 1 }));
+				this.updateAnswerInput();
 			});
 
-			$("#" +  this.answerInputId).val(buckets.join(","));
-		}
+			el.querySelector('.dd-reset-buckets')?.addEventListener('click', () => {
+				for (const bucket of this.buckets) {
+					bucket.el.remove();
+				}
+				this.buckets = [];
 
-		_ddUpdate() {
-			const answerInputId = this.answerInputId;
-			const $bucketPool = $('.dd-bucket-pool[data-ans="' + answerInputId + '"]').first();
-			const el = this;
-
-			$(function() {
-				$bucketPool.parent().find('.dd-add-bucket').off();
-				$bucketPool.parent().find('.dd-add-bucket').on('click', function() {
-					new DragNDropBucket({
-						answerInputId: $(this).attr('data-ans'),
-						bucketId: +($('.dd').length) + 1,
-						removable: 1,
-						label:'',
-					});
-				});
-				$bucketPool.find('.dd-remove-bucket').off();
-				$bucketPool.find('.dd-remove-bucket').on('click', function() {
-					if ($bucketPool.find('.dd ol').length == 1) {
-						return 0;
-					}
-					const $container = $(this).closest('.dd-container');
-
-					$container.find('li').appendTo($bucketPool.find('.dd ol').first());
-					$container.remove();
-					el._nestableUpdate();
-				});
-				$bucketPool.parent().find('.dd-reset-buckets').off();
-				$bucketPool.parent().find('.dd-reset-buckets').on('click', function() {
-					$bucketPool.find('.dd-container').remove();
-					$bucketPool.find('div.dd-hidden.dd-default.dd-bucket').each(function() {
-						const bucketId = $(this).attr('data-bucket-id');
-						const $bucket = el._newBucket(
-							$(this).attr('data-bucket-id'),
-							$(this).find('.dd-label').first().html(),
-							$(this).attr('data-removable'),
-							$bucketPool.find('.dd-hidden.dd-default.dd-bucket[data-bucket-id="' + bucketId + '"]')
-						);
-
-						$bucketPool.append($bucket);
-					});
-
-					$bucketPool.find('.dd').nestable({
-						group: el.answerInputId,
-						maxDepth: 1,
-						scroll: true,
-						callback: function() {el._nestableUpdate();}
-					});
-					el._nestableUpdate();
-				});
+				for (const bucket of this.defaultState) {
+					this.buckets.push(new Bucket(this, this.buckets.length, bucket));
+				}
+				this.updateAnswerInput();
 			});
 		}
 
+		updateAnswerInput() {
+			const contents = [];
+
+			for (const bucket of this.buckets) {
+				const list = bucket.sortable.toArray();
+				contents.push(`{${list.join(',')}}`);
+			}
+
+			this.answerInput.value = '(' + contents.join(',') + ')';
+		}
 	}
 
-	$('div.dd-bucket-pool').each(function() {
-		const answerInputId = $(this).attr('data-ans');
+	class Bucket {
+		constructor(bucketPool, id, bucketData) {
+			this.id = id;
+			this.bucketPool = bucketPool;
 
-		if ($(this).find('div.dd-bucket.dd-past-answers.dd-hidden').length) {
-			$(this).find('div.dd-bucket.dd-past-answers.dd-hidden').each(function() {
-				new DragNDropBucket({
-					answerInputId : answerInputId,
-					bucketId : $(this).attr('data-bucket-id'),
-					label : $(this).find('.dd-label').html(),
-					removable : $(this).attr('data-removable'),
-				});
+			this.el = this.htmlBucket(bucketData.label, bucketData.removable, bucketData.indices);
+			bucketPool.bucketContainer.append(this.el);
+
+			// Typeset any math content that may be in the added html.
+			if (window.MathJax) {
+				MathJax.startup.promise = MathJax.startup.promise.then(() => MathJax.typesetPromise([this.el]));
+			}
+
+			this.sortable = Sortable.create(this.ddList, {
+				group: bucketPool.answerName,
+				animation: 150,
+				onEnd: () => this.bucketPool.updateAnswerInput()
 			});
 		}
-	});
 
+		htmlBucket(label, removable, indices = []) {
+			const bucketElement = document.createElement('div');
+			bucketElement.classList.add('dd-bucket');
+
+			const bucketLabel = document.createElement('div');
+			bucketLabel.classList.add('dd-bucket-label');
+			bucketLabel.innerHTML =
+				label || (this.bucketPool.labelFormat ? `${this.bucketPool.labelFormat.replace(/%s/, this.id + 1)}` : '');
+
+			this.ddList = document.createElement('div');
+			this.ddList.classList.add('dd-list');
+
+			bucketElement.append(bucketLabel, this.ddList);
+
+			for (const index of indices) {
+				if (index < 0 || index > this.bucketPool.itemList.length) continue;
+
+				const listElement = document.createElement('div');
+				listElement.classList.add('dd-item');
+				listElement.dataset.id = index;
+				listElement.innerHTML = this.bucketPool.itemList[index];
+
+				this.ddList.append(listElement);
+			}
+
+			bucketElement.style.backgroundColor = `hsla(${(100 + this.id * 100) % 360}, 40%, 90%, 1)`;
+
+			// The first bucket is not allowed to be removable.
+			if (this.id !== 0 && removable) {
+				const removeButton = document.createElement('button');
+				removeButton.type = 'button';
+				removeButton.classList.add('btn', 'btn-secondary', 'dd-remove-bucket-button');
+				removeButton.textContent = this.bucketPool.removeButtonText;
+
+				removeButton.addEventListener('click', () => {
+					const firstBucketList = this.bucketPool.buckets[0].ddList;
+					for (const item of this.ddList.querySelectorAll('.dd-item')) {
+						firstBucketList.append(item);
+					}
+
+					bucketElement.remove();
+					const index = this.bucketPool.buckets.findIndex((bucket) => bucket.id === this.id);
+					if (index !== -1) this.bucketPool.buckets.splice(index, 1);
+					this.bucketPool.updateAnswerInput();
+				});
+
+				bucketElement.append(removeButton);
+			}
+
+			return bucketElement;
+		}
+	}
+
+	// Set up bucket pools that are already in the page.
+	for (const bucketPoolEl of document.querySelectorAll('.dd-bucket-pool')) {
+		new BucketPool(bucketPoolEl);
+	}
+
+	// Set up bucket pools that are added to the page later.
+	const observer = new MutationObserver((mutationsList) => {
+		for (const mutation of mutationsList) {
+			for (const node of mutation.addedNodes) {
+				if (node instanceof Element) {
+					if (node.classList.contains('dd-bucket-pool')) {
+						if (!node.dataset.bucketPoolInitialized) new BucketPool(node);
+					} else {
+						for (const bucketPoolEl of node.querySelectorAll('.dd-bucket-pool')) {
+							if (bucketPoolEl.dataset.bucketPoolInitialized) continue;
+							new BucketPool(bucketPoolEl);
+						}
+					}
+				}
+			}
+		}
+	});
+	observer.observe(document.body, { childList: true, subtree: true });
+
+	// Stop the mutation observer when the window is closed.
+	window.addEventListener('unload', () => observer.disconnect());
 })();
