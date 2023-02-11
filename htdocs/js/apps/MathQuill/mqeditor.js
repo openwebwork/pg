9d92e67ca370ceae14183488a7c7d2c51a67b07b
Glenn Rice <grice1@missouriwestern.edu>
2023-02-11 09:13:44 -0600

    Rework the mqeditor toolbar postioning and behavior
    The toolbar is absolutely positioned relative to the position of the
    answerQuill input on the page that it corresponds to.  This means that
    the toolbar will stay with the input when the page scrolls.  If the
    problem is rendered in an iframe, then the toolbar is still always to
    the right of the input.  Care is taken to ensure that the toolbar
    doesn't go off the top or bottom of the page (unless the page is too
    short to accommodate the toolbar height -- see below).
    
    The mqeditor toolbar is now scrollable when in short pages.  The toolbar
    does not collapse into multiple columns, and is always a single column.
    So it doesn't intrude into the problem when the page is short anymore
    than its single column 53px width (plus a 10 pixel gap to the right of
    the toolbar).
    
    In order for the toolbar to be scrollable, it needs to be able to attain
    focus.  Otherwise it will disappear when the user tries to scroll it
    using the scrollbar.  So the toolbar is inserted into the page
    immediately after the MathQuill input.  This means that if the MathQuill
    input has focus and the tab button is pressed, then focus will go to the
    first button of the toolbar (and then on through the buttons when tab is
    pressed additional times).  If you tab all the way through the buttons,
    then focus will continue in the page to the next place it would have
    gone if the toolbar wasn't there.  Note that the toolbar is not in the
    reverse tab order when using shift-tab from somewhere after the
    MathQuill input (since it doesn't exist at that time).  This new tab
    order approach is probably more accessible in any case, since the
    toolbar now can be used with the keyboard.
    
    Finally, if you don't want the toolbar to open for you at all, then type
    alt-t when any MathQuill input has focus.  The toolbar will hide, and
    will not open again for any MathQuill input.  If you type alt-t again in
    any MathQuill input, then the toolbar will open and thereafter will
    continue to open for all MathQuill inputs.  This setting is saved in
    local storage, and so will persist for any page on the site. I.e., alt-t
    typed in any MathQuill input toggles whether the toolbar is enabled or
    not.

--- htdocs/js/apps/MathQuill/mqeditor.js
+++ htdocs/js/apps/MathQuill/mqeditor.js
@@ -9,6 +9,8 @@
 	// initialize MathQuill
 	const MQ = MathQuill.getInterface(2);
 
+	let toolbarEnabled = (localStorage.getItem('MQEditorToolbarEnabled') ?? 'true') === 'true';
+
 	const setupMQInput = (mq_input) => {
 		const answerLabel = mq_input.id.replace(/^MaThQuIlL_/, '');
 		const input = document.getElementById(answerLabel);
@@ -86,17 +88,34 @@
 
 		answerQuill.hasFocus = false;
 
+		const toolbarRemove = () => {
+			if (!answerQuill.hasFocus && answerQuill.toolbar) {
+				window.removeEventListener('resize', answerQuill.toolbar.setPosition);
+				answerQuill.toolbar.tooltips.forEach((tooltip) => tooltip.dispose());
+				answerQuill.toolbar.remove();
+				delete answerQuill.toolbar;
+			}
+		};
+
 		// Open the toolbar when the mathquill answer box gains focus.
 		answerQuill.textarea.addEventListener('focusin', () => {
+			if (!toolbarEnabled) return;
 			answerQuill.hasFocus = true;
 			if (answerQuill.toolbar) return;
 
 			answerQuill.toolbar = document.createElement('div');
+			answerQuill.toolbar.tabIndex = -1;
 			answerQuill.toolbar.classList.add('quill-toolbar');
+
+			answerQuill.toolbar.addEventListener('focusin', (e) => answerQuill.hasFocus = true);
+			answerQuill.toolbar.addEventListener('focusout', (e) => {
+				answerQuill.hasFocus = false;
+				setTimeout(toolbarRemove, 200);
+			});
 
 			answerQuill.toolbar.tooltips = [];
 
-			answerQuill.buttons.forEach((buttonData) => {
+			for (const buttonData of answerQuill.buttons) {
 				const button = document.createElement('button');
 				button.type = 'button';
 				button.id = `${buttonData.id}-${answerQuill.id}`;
@@ -121,34 +140,61 @@
 					answerQuill.mathField.cmd(button.dataset.latex);
 					answerQuill.textarea.focus();
 				})
-			});
-			document.body.append(answerQuill.toolbar);
+			}
 
-			// This is covered by css for the standard toolbar sizes.  However, if buttons are added or removed from the
-			// toolbar by the problem or if the window height is excessively small, those may be incorrect.  So this
-			// adjusts the width in those cases.
-			answerQuill.toolbar.adjustWidth = () => {
-				if (!answerQuill.toolbar) return;
-				const left =
-					answerQuill.toolbar.querySelector('.symbol-button:first-child')?.getBoundingClientRect().left ?? 0;
-				const right =
-					answerQuill.toolbar.querySelector('.symbol-button:last-child')?.getBoundingClientRect().right ?? 0;
-				answerQuill.toolbar.style.width = `${right - left + 8}px`;
-			};
-			window.addEventListener('resize', answerQuill.toolbar.adjustWidth);
-			setTimeout(answerQuill.toolbar.adjustWidth);
+			answerQuill.toolbar.setPosition = () => {
+				// Note that this must be kept in sync with css.  Currently each symbol button has a fixed height (due
+				// to flex-shrink being 0) of 45px plus a 1px padding on the top and bottom plus a 1px margin on the top
+				// and bottom, giving a 49px total height for each symbol button .  Also, the toolbar itself has a 2px
+				// border on the top and bottom, hence 4px is added to the end.  These computations take into account
+				// that box-sizing is border-box.
+				const toolbarHeight = 49 * answerQuill.buttons.length + 4;
+
+				// Get the page height.  This is the greater of the height of the document element or the window height.
+				const pageHeight = (() => {
+					const documentElHeight = document.documentElement.getBoundingClientRect().height;
+					if (window.innerHeight > documentElHeight) return window.innerHeight;
+					return documentElHeight;
+				})();
+
+				if (toolbarHeight > pageHeight) {
+					answerQuill.toolbar.style.top = 0;
+					answerQuill.toolbar.style.height = '100%';
+				} else {
+					const elRect = answerQuill.getBoundingClientRect();
+					const top = window.scrollY + elRect.bottom - elRect.height / 2 - toolbarHeight / 2;
+					const bottom = window.scrollY + elRect.bottom - elRect.height / 2 + toolbarHeight / 2;
+					answerQuill.toolbar.style.top =
+						`${top < 0 ? 0 : bottom > pageHeight ? pageHeight - toolbarHeight : top}px`;
+					answerQuill.toolbar.style.height = null;
+				}
+			}
+
+			window.addEventListener('resize', answerQuill.toolbar.setPosition);
+			answerQuill.toolbar.setPosition();
+
+			answerQuill.after(answerQuill.toolbar);
 		});
 
-		answerQuill.textarea.addEventListener('focusout', (e) => {
-			answerQuill.hasFocus = false;
-			setTimeout(function() {
-				if (!answerQuill.hasFocus && answerQuill.toolbar) {
-					window.removeEventListener('resize', answerQuill.toolbar.adjustWidth);
-					answerQuill.toolbar.tooltips.forEach((tooltip) => tooltip.dispose());
-					answerQuill.toolbar.remove();
-					delete answerQuill.toolbar;
+		// Alt-t toggles whether the toolbar is enabled or not.
+		answerQuill.addEventListener('keydown', (e) => {
+			if (e.key === 't' && e.altKey) {
+				e.preventDefault();
+				toolbarEnabled = !toolbarEnabled;
+				localStorage.setItem('MQEditorToolbarEnabled', toolbarEnabled)
+				if (!toolbarEnabled && answerQuill.toolbar) {
+					answerQuill.hasFocus = false;
+					toolbarRemove();
 				}
-			}, 200);
+				if (toolbarEnabled && !answerQuill.toolbar) {
+					answerQuill.textarea.dispatchEvent(new Event('focusin'));
+				}
+			}
+		});
+
+		answerQuill.textarea.addEventListener('focusout', () => {
+			answerQuill.hasFocus = false;
+			setTimeout(toolbarRemove, 200);
 		});
 
 		// Trigger an answer preview when the enter key is pressed in an answer box.
@@ -177,7 +223,7 @@
 		answerQuill.mathField.blur();
 
 		// Look for a result in the attempts table for this answer.
-		document.querySelectorAll('td a[data-answer-id]').forEach((tableLink) => {
+		for (const tableLink of document.querySelectorAll('td a[data-answer-id]')) {
 			// Give the mathquill answer box the correct/incorrect colors.
 			if (answerLabel.includes(tableLink.dataset.answerId)) {
 				if (tableLink.parentNode.classList.contains('ResultsWithoutError'))
@@ -192,7 +238,7 @@
 					answerQuill.textarea.focus();
 				});
 			}
-		});
+		}
 
 		window.answerQuills[answerLabel] = answerQuill;
 	};
@@ -202,8 +248,8 @@
 
 	// Observer that sets up MathQuill inputs.
 	const observer = new MutationObserver((mutationsList) => {
-		mutationsList.forEach((mutation) => {
-			mutation.addedNodes.forEach((node) => {
+		for (const mutation of mutationsList) {
+			for (const node of mutation.addedNodes) {
 				if (node instanceof Element) {
 					if (node.id && node.id.startsWith('MaThQuIlL_')) {
 						setupMQInput(node);
@@ -211,8 +257,8 @@
 						node.querySelectorAll('input[id^=MaThQuIlL_]').forEach((input) => setupMQInput(input));
 					}
 				}
-			});
-		});
+			}
+		}
 	});
 	observer.observe(document.body, { childList: true, subtree: true });
 
