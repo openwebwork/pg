621527944ba286677ed0f39a87e2338b9d43c9a3
Alex Jordan <alex.jordan@pcc.edu>
2023-05-07 21:17:47 -0700

    fix vertical rules in hardcopy

--- macros/ui/niceTables.pl
+++ macros/ui/niceTables.pl
@@ -891,8 +891,14 @@ sub Row {
 				$columntype =~ s/^p/b/ if ($valign eq 'bottom');
 				$columntype =~ s/^p/m/ if ($tableOpts->{valign} eq 'middle');
 				$columntype =~ s/^p/b/ if ($tableOpts->{valign} eq 'bottom');
-				$columntype .= '|'
-					if ($i == 0 && $cellOpts->{colspan} == 1 && $tableOpts->{rowheaders} && $tableOpts->{headerrules});
+				$columntype = getLaTeXcolumnWidth($alignment->[0]{left}) . $columntype
+					if ($i == 0 && $alignment->[0]{left} && !$cellOpts->{halign});
+
+				if ($i == 0 && $cellOpts->{colspan} == 1 && $tableOpts->{rowheaders} && $tableOpts->{headerrules}) {
+					$columntype .= '|';
+				} elsif (!$cellOpts->{halign}) {
+					$columntype .= getLaTeXcolumnWidth($cellAlign->{right});
+				}
 				$cell = latexCommand('multicolumn', [ $cellOpts->{colspan}, $columntype, $cell ]);
 			}
 			$cell = suffix($cell, '&', ' ') unless ($i == $#$rowArray);
@@ -1430,6 +1436,16 @@ sub getLaTeXthickness {
 		$output = "$input";
 	}
 	return $output;
+}
+
+sub getLaTeXcolumnWidth {
+	my $input = shift;
+	return '' unless $input;
+	if ($input =~ /^\d+$/) {
+		return '|' x $input;
+	} else {
+		return "!{\\vrule width $input}";
+	}
 }
 
 sub getRuleCSS {
