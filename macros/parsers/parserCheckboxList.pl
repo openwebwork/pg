70e77054041fd24621096807f6d119ff0de63e9d
Alex Jordan <alex.jordan@pcc.edu>
2024-07-08 23:08:53 -0700

    add showInStatic option to radio buttons and checkbox list

--- macros/parsers/parserCheckboxList.pl
+++ macros/parsers/parserCheckboxList.pl
@@ -170,6 +170,12 @@ Determines whether a numeric value for the correct answer is interpreted as an
 index into the choice array or not.  If set to 1, then the number is treated as
 the literal correct answer, not an index to it.  Default: 0
 
+=item C<S<< showInStatic => 0 or 1 >>>
+In static output, such as PDF or PTX, this controls whether or not
+the list of answer options is displayed.  (The text preceding the list
+of answer options might make printing the answer option list
+unnecessary in a static output format.)  Default: 1
+
 =back
 
 To insert the checkboxes into the problem text use
@@ -244,6 +250,7 @@ sub new {
 		noindex          => 0,
 		checkedI         => [],
 		localLabels      => 0,
+		showInStatic     => 1,
 		@inputs
 	);
 	$options{originalLabels} = $options{labels};
@@ -570,37 +577,43 @@ sub CHECKS {
 	}
 
 	if ($main::displayMode eq 'TeX') {
-		$checks[0] = "\n\\begin{itemize}\n" . $checks[0];
-		$checks[-1] .= "\n\\end{itemize}\n";
-	} elsif ($main::displayMode eq 'PTX') {
-
-		# Do we want an ol, ul, or dl?
-		my $list_type      = 'ul';
-		my $subtype        = '';
-		my $originalLabels = $self->{originalLabels};
-		if ($originalLabels =~ m/^(123|abc|roman)$/i) {
-			my $marker = '';
-			$marker = "1" if $originalLabels eq '123';
-			$marker = "a" if $originalLabels eq 'abc';
-			$marker = "A" if uc($originalLabels) eq 'ABC' && $originalLabels ne 'abc';
-			$marker = "i" if $originalLabels eq 'roman';
-			$marker = "I" if uc($originalLabels) eq 'ROMAN' && $originalLabels ne 'roman';
-
-			$list_type  = 'ol';
-			$subtype    = qq( marker="$marker");
-			$close_list = 'ol';
-		} elsif ($self->{localLabels} || ref $originalLabels eq 'ARRAY') {
-			$list_type = 'dl';
-			$subtype   = ' width = "narrow"';
-			my %checks_to_labels = map { $checks[$_] => $self->{labels}[$_] } (0 .. $#{ $self->{orderedChoices} });
-			map { $_ =~ s/^(<li.*?>)/$1<title>$checks_to_labels{$_}<\/title>/gr } @checks;
+		if ($self->{showInStatic}) {
+			$checks[0] = "\n\\begin{itemize}\n" . $checks[0];
+			$checks[-1] .= "\n\\end{itemize}\n";
+		} else {
+			@checks = ();
 		}
-		$checks[0] = qq{<$list_type$subtype name="$name">\n$checks[0]};
-		$checks[-1] .= "</$list_type>";
+	} elsif ($main::displayMode eq 'PTX') {
+		if ($self->{showInStatic}) {
+			# Do we want an ol, ul, or dl?
+			my $list_type      = 'ul';
+			my $subtype        = '';
+			my $originalLabels = $self->{originalLabels};
+			if ($originalLabels =~ m/^(123|abc|roman)$/i) {
+				my $marker = '';
+				$marker = "1" if $originalLabels eq '123';
+				$marker = "a" if $originalLabels eq 'abc';
+				$marker = "A" if uc($originalLabels) eq 'ABC' && $originalLabels ne 'abc';
+				$marker = "i" if $originalLabels eq 'roman';
+				$marker = "I" if uc($originalLabels) eq 'ROMAN' && $originalLabels ne 'roman';
 
-		# Change math delimiters
-		@checks = map { $_ =~ s/\\\(/<m>/gr } @checks;
-		@checks = map { $_ =~ s/\\\)/<\/m>/gr } @checks;
+				$list_type = 'ol';
+				$subtype   = qq( marker="$marker");
+			} elsif ($self->{localLabels} || ref $originalLabels eq 'ARRAY') {
+				$list_type = 'dl';
+				$subtype   = ' width = "narrow"';
+				my %checks_to_labels = map { $checks[$_] => $self->{labels}[$_] } (0 .. $#{ $self->{orderedChoices} });
+				map { $_ =~ s/^(<li.*?>)/$1<title>$checks_to_labels{$_}<\/title>/gr } @checks;
+			}
+			$checks[0] = qq{<$list_type$subtype name="$name">\n$checks[0]};
+			$checks[-1] .= "</$list_type>";
+
+			# Change math delimiters
+			@checks = map { $_ =~ s/\\\(/<m>/gr } @checks;
+			@checks = map { $_ =~ s/\\\)/<\/m>/gr } @checks;
+		} else {
+			@checks = ();
+		}
 	} else {
 		$checks[0] =
 			qq{<div class="checkboxes-container" }
