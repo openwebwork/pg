f22c1e21d5ce0858d417c4c3f8e9ed727bd5582b
Glenn Rice <grice1@missouriwestern.edu>
2025-03-25 16:30:51 -0500

    Fix a vulnerability in `Statistics.pm` and the macro `PGstatisticsmacros.pl`.
    This vulnerability allows a problem to write to any location or
    overwrite any file that the server has write permission to.
    
    The problem is that the `write_array_to_CSV` method of the
    `Statistics.pm` module takes a file name as its first argument and
    writes to that file without question.
    
    To fix this the method instead uses the new `WeBWorK::PG::IO::saveDataToFile`
    method which refuses to write anything outside of the html temporary
    directory.
    
    Note that the `make_csv_alias` method of the `Statistics.pm` module was
    dropped because it uses an invalid approach to creating a unique file
    name and is not needed.  So there is no need for the `Statistics`
    package to even be an object.  Now there is just the method
    `write_array_to_CSV`.
    
    Previously the `insertDataLink` method of `PGstatisticsmacros.pl`
    accepted the `$PG` object for its first argument.  That first argument
    is still accepted, but is dropped if given, and there is no need to pass
    that argument anymore. The macro has direct access to the `$PG` variable
    so it is not a needed parameter.
    
    Also, a new optional last argument to the `insertDataLink` is accepted.
    If provide this argument must be a reference to a hash, and the
    key/value pairs in that hash will be added as attributes to the HTML `a`
    tak that is returned. If this parameter is not provided or it is but
    does not contain a `download` attribute, then a default `download`
    attribute with value `data.csv` will be added.
    
    Note that the `insertDataLink` method is not used by any problem in the
    OPL or Contrib.  Furthermore, nothing in the `Statistics.pm` module is
    directly used by any problem in OPL or Contrib. Backwards compatibility
    is maintained for the `insertDataLink` method for anyone that may have
    been using it, but not for the `Statistics.pm` module.  It was not
    intended that the module be used directly anyway.
    
    This fixes the second of the three ways that I know of for a problem to
    directly write to anywhere that the server has permission to do so.

--- lib/Statistics.pm
+++ lib/Statistics.pm
@@ -16,108 +16,36 @@
 
 package Statistics;
 
-#use strict;
-use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
-require Exporter;
-@ISA       = qw(Exporter);
-@EXPORT_OK = qw( );
-@EXPORT    = qw( );
-$VERSION   = '0.01';
+use strict;
+use warnings;
 
-# ##########################################
-# Initialize the class
-# Expected argument: a ref to a valid PGcore object
-#
-sub new {
-	my $class = shift;
-	my $self  = { PG => shift };
+require WeBWorK::PG::IO;
 
-	bless $self, $class;
-
-}
-
-# ##########################################
-# Create an alias to a csv file.
-# Return the url that can be used in a browser
-# to access the file.
-#
-sub make_csv_alias {
-
-	my $self         = shift;
-	my $studentLogin = shift;
-	my $problemSeed  = shift;
-	my $setname      = shift;
-	my $prob         = shift;
-
-	# Clean the student login string to make it appropriate for a file name.
-	$studentLogin =~ s/Q/QQ/g;
-	$studentLogin =~ s/\./-Q-/g;
-	$studentLogin =~ s/\,/-Q-/g;
-	$studentLogin =~ s/\@/-Q-/g;
-
-	# Define the file name, clean it up and convert to a url.
-	my $filePath = "data/$studentLogin-$problemSeed-set" . $setName . "prob$prob.html";
-	$filePath = $self->{PG}->surePathToTmpFile($filePath);
-	my $url = $self->{PG}->{PG_alias}->make_alias($filePath);
-
-	# Remove the .html off the end and replace it with a .csv
-	$filePath =~ s/\.html$/.csv/;
-	$url      =~ s/\.html$/.csv/;
-
-	($filePath, $url);
-}
-
-# ##########################################
 # Write the given data to a csv file.
-# Return the URL if the file is written,
-# an empty string otherwise
-#
 sub write_array_to_CSV {
-	my $self     = shift;
-	my $fileName = shift;
-	my @dataRefs = @_;
+	my ($fileName, @dataRefs) = @_;
+
+	die 'No data set was provided.' unless @dataRefs && ref $dataRefs[0] eq 'ARRAY';
 
 	# Make sure all of the data sets have the same number of elements
-	my $numberDataPoints = "nd";
-	my $data;
-	foreach $data (@dataRefs) {
-		my @dataArray = @{$data};
-		if ($numberDataPoints eq "nd") {
-			$numberDataPoints = $#dataArray;
-		} elsif ($numberDataPoints != $#dataArray) {
-			die("$0", "The number of elements in the data sets are not all the same. No data set written to file.");
-			return;
-		}
+	my $numberDataPoints = $#{ $dataRefs[0] };
+	for my $data (@dataRefs) {
+		die 'The number of elements in the data sets are not all the same. No data set written to file.'
+			if $numberDataPoints != $#$data;
 	}
 
-	# Open the file
-	local (*OUTPUT);    # create local file handle so it won't overwrite other open files.
-	open(OUTPUT, ">$fileName") || warn("$0", "Can't open $fileName<BR>", "");
-	chmod(0777, $filePath);
+	# Add the header to the first row of the output.
+	my $output = join(',', map { $_->[-1] } @dataRefs) . "\n";
 
-	#  write the header to the first row.
-	my $header = "";
-	foreach $data (@dataRefs) {
-		$header .= pop(@{$data}) . ",";
-	}
-	$header =~ s/,$//;
-	print OUTPUT ($header . "\n") || warn("$0", "Can't print data file to $fileName<BR>", "");
-
-	# Go through each data point and write it out.
-	my $lupe;
-	for ($lupe = 0; $lupe < $numberDataPoints; ++$lupe) {
-		my $line = "";
-		foreach $data (@dataRefs) {
-			my @dataSet = @{$data};
-			$line .= $dataSet[$lupe] . ",";
-		}
-		$line =~ s/,$//;
-		print OUTPUT ($line, "\n");
+	# Add each data point as another row in the output.
+	for my $i (0 .. $numberDataPoints - 1) {
+		$output .= join(',', map { $_->[$i] } @dataRefs) . "\n";
 	}
 
-	# Close it up and move on.
-	close(OUTPUT) || warn("$0", "Can't close $filePath<BR>", "");
+	# Write the output to disk.
+	WeBWorK::PG::IO::saveDataToFile($output, $fileName);
 
+	return;
 }
 
 1;
